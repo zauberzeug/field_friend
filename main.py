c4d2ea825a7a973aa3ec008bacc6ed534715e4ce
Rodja Trappe <rodja@zauberzeug.com>
2022-11-13 06:39:33 +0100

    show extra ui for development/debugging

--- main.py
+++ main.py
@@ -27,8 +27,14 @@ async def index():
     ui.colors(primary='#6E93D6', secondary='#53B689', accent='#111B1E', positive='#53B689')
     interface.navigation_bar(robot)
 
-    with ui.column().classes('w-full no-wrap items-stretch q-px-md'):
-        with ui.row().classes('items-stretch justify-items-stretch').style('flex-wrap:nowrap'):
-            interface.operation(steerer, automator, odometer)
+    with ui.row().classes('items-stretch justify-items-stretch').style('flex-wrap:nowrap'):
+        interface.operation(steerer, automator, odometer)
+    with ui.card():
+        if robot.is_real:
+            robot.robot_brain.developer_ui()
+            robot.robot_brain.communication.debug_ui()
+            rosys.on_startup(robot.robot_brain.lizard_firmware.ensure_lizard_version)
+        else:
+            rosys.simulation_ui()
 
-ui.run(port=80 if robot.is_real else 8080)
+ui.run(title='Field Friend', port=80 if robot.is_real else 8080)
