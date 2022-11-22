6d920b61bd993e75c0006f28898fd989d91094a6
Miguel <angelom93@web.de>
2022-11-22 09:29:27 +0100

    added detector and automation to main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import rosys
 from nicegui import ui
 
+import automations
 import hardware
 import interface
 import log
@@ -14,15 +15,20 @@ if is_real:
     robot_brain = rosys.hardware.RobotBrain(communication)
     robot = hardware.RobotHardware(robot_brain)
     usb_camera_provider = rosys.vision.UsbCameraProviderHardware()
+    detector = rosys.vision.DetectorHardware
 else:
     robot = hardware.RobotSimulation()
     usb_camera_provider = rosys.vision.UsbCameraProviderSimulation()
+    detector = rosys.vision.DetectorSimulation
+coin_provider = automations.CoinProvider()
 steerer = rosys.driving.Steerer(robot, speed_scaling=0.2)
 odometer = rosys.driving.Odometer(robot)
 driver = rosys.driving.Driver(robot, odometer)
-driver.parameters.linear_speed_limit = 0.5
-driver.parameters.angular_speed_limit = 0.5
+driver.parameters.linear_speed_limit = 0.2
+driver.parameters.angular_speed_limit = 0.2
 automator = rosys.automation.Automator(robot, steerer)
+coin_collecting = automations.CoinCollecting(robot, driver, detector, usb_camera_provider, coin_provider)
+automator.default_automation = coin_collecting.start()
 
 
 @ui.page('/', shared=True)
@@ -32,7 +38,7 @@ async def index():
 
     with ui.row().classes('fit items-stretch justify-around').style('flex-wrap:nowrap'):
         interface.operation(steerer, automator, odometer, usb_camera_provider)
-        interface.camera(usb_camera_provider, automator, robot)
+        interface.camera(usb_camera_provider, automator, robot, coin_collecting)
     interface.development(robot, automator)
 
 if robot.is_simulation:
