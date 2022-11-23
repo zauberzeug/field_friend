12c9ceffe4e07181b75f710268752007d75991bf
Rodja Trappe <rodja@zauberzeug.com>
2022-11-23 08:27:51 +0100

    compatibility with latest rosys

--- interface/development.py
+++ interface/development.py
@@ -22,7 +22,6 @@ def development(robot: hardware.robot.Robot, automator: rosys.automation.automat
                         ui.menu_item('Enable end stops', on_click=lambda: automator.start(robot.enable_end_stops(True)))
                     ui.button(on_click=developer_menu.open).props('dense fab-mini outline icon=more_vert')
                     robot_status = ui.markdown()
-            rosys.on_startup(robot.robot_brain.lizard_firmware.ensure_lizard_version)
             ui.timer(1, lambda: robot_status.set_content(
                 f' yaxis: Alarm: {robot.yaxis_alarm} Idle: {robot.yaxis_idle} Pos: {robot.yaxis_position} Home: {robot.yaxis_home_position} Ref:{robot.yaxis_is_referenced} end l: {robot.yaxis_end_l} end r: {robot.yaxis_end_r}<br>'
                 f'zaxis: Alarm: {robot.zaxis_alarm} Idle: {robot.zaxis_idle} Pos: {robot.zaxis_position} Home: {robot.zaxis_home_position} Ref:{robot.zaxis_is_referenced} end t: {robot.zaxis_end_t} end b: {robot.zaxis_end_b}'
