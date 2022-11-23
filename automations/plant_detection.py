8e93e89d57514f808932d0e2abc073d531cc5f41
Miguel <angelom93@web.de>
2022-11-23 12:59:30 +0100

    plant detection uses camera_detector to get camera

--- automations/plant_detection.py
+++ automations/plant_detection.py
@@ -19,21 +19,19 @@ class DetectorError(Exception):
 
 class PlantDetection:
 
-    def __init__(self, detector: rosys.vision.Detector, camera_provider: rosys.vision.CameraProvider,
-                 plant_provider: PlantProvider, robot: Robot) -> None:
+    def __init__(self, detector: rosys.vision.Detector, plant_provider: PlantProvider, robot: Robot) -> None:
         self.detector = detector
-        self.camera_provider = camera_provider
         self.plant_provider = plant_provider
         self.robot = robot
         self.log = logging.getLogger('field_friend.plant_detection')
 
-    async def check_cam(self) -> None:
+    async def check_cam(self, camera: rosys.vision.Camera) -> None:
         self.log.info('Detecting in cam')
-        detections = await self.detect()
-        await self.update_plants(detections, list(self.camera_provider.cameras.values())[0].calibration)
+        detections = await self.detect(camera)
+        await self.update_plants(detections, camera)
 
-    async def detect(self) -> rosys.vision.Detections:
-        self.image = self.camera_provider.images[0]
+    async def detect(self, camera: rosys.vision.Camera) -> rosys.vision.Detections:
+        self.image = camera.latest_captured_image
         self.log.info(f'getting last captured image')
         if self.image is None:
             self.log.info('no image found')
@@ -44,25 +42,27 @@ class PlantDetection:
             raise DetectorError()
         return self.image.detections
 
-    async def update_plants(self, detection: rosys.vision.Detections, calibration: rosys.vision.Calibration) -> None:
+    async def update_plants(self, detection: rosys.vision.Detections, camera: rosys.vision.Camera) -> None:
+        self.calibration = camera.calibration
+        if self.calibration is None:
+            rosys.notify('camera has no calibration')
+            raise DetectorError()
         weed_detections = [
             d for d in detection.points
             if d.category_name in WEED_CATEGORY_NAME and d.confidence >= MINIMUM_WEED_CONFIDENCE]
         if not weed_detections:
             return
-        point2d = [
+        weed_image_positions = [
             rosys.geometry.Point(x=d.cx, y=d.cy) for d in weed_detections
         ]
-        self.log.info(f'my points in 2d {point2d}')
-        weed_positions: list[Point3d] = [
-            calibration.project_from_image(point2d[0])  # for p in point2d
+        weed_world_positions: list[Point3d] = [
+            self.calibration.project_from_image(w) for w in weed_image_positions  # for p in point2d
         ]
-        self.log.info(f'my points after calibration {weed_positions}')
         weeds: list[Plant] = [
             Plant(position=Point(x=p.x, y=p.y), id=weed_detections[i].uuid, type='weed', mac='detected', detection_time=rosys.time())
-            for i, p in enumerate(weed_positions)
+            for i, p in enumerate(weed_world_positions)
         ]
-        self.log.info(f'found weeds: {weeds}')
+        self.log.info(f'found {len(weeds)} weeds')
         await self.plant_provider.add_weed(*weeds)
         self.log.info(f'weed_provider: {self.plant_provider.weeds}')
 
