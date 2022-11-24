f5c69127ee3af99cca13bcfbd81d17a1a620ba3d
Miguel <angelom93@web.de>
2022-11-24 16:02:13 +0100

    driver parameters

--- main.py
+++ main.py
@@ -25,8 +25,9 @@ plant_provider = automations.PlantProvider()
 steerer = rosys.driving.Steerer(robot, speed_scaling=0.2)
 odometer = rosys.driving.Odometer(robot)
 driver = rosys.driving.Driver(robot, odometer)
-driver.parameters.linear_speed_limit = 0.1
-driver.parameters.angular_speed_limit = 0.1
+driver.parameters.linear_speed_limit = 0.2
+driver.parameters.angular_speed_limit = 0.2
+driver.parameters.can_drive_backwards = False
 automator = rosys.automation.Automator(robot, steerer)
 weeding = automations.Weeding(robot, driver, detector, camera_selector, plant_provider)
 automator.default_automation = weeding.start
