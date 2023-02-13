10d5efee85c199834565f7b02dda235ce5cf9988
Miguel <angelom93@web.de>
2023-02-13 17:21:01 +0100

    use of rosys.backgroundtasks in key controlls

--- field_friend/interface/key_controls.py
+++ field_friend/interface/key_controls.py
@@ -1,48 +1,54 @@
 import rosys
 from nicegui.events import KeyEventArguments
+from rosys import background_tasks
 from rosys.automation import Automator
 from rosys.driving import Steerer
+from rosys.hardware import Wheels
 
+from ..hardware import EStop, YAxis, ZAxis
 from ..old_hardware import Robot
 
 
 class KeyControls(rosys.driving.keyboard_control):
 
-    def __init__(self, robot: Robot, steerer: Steerer, automator: Automator) -> None:
+    def __init__(self, wheels: Wheels, y_axis: YAxis, z_axis: ZAxis, steerer: Steerer, automator: Automator) -> None:
         super().__init__(steerer)
 
-        self.robot = robot
+        self.wheels = wheels
+        self.y_axis = y_axis
+        self.z_axis = z_axis
         self.automator = automator
 
     def handle_keys(self, e: KeyEventArguments) -> None:
         super().handle_keys(e)
 
-        if e.modifiers.shift and e.action.keydown:
-            if e.key == '!':
-                async def try_axis_home():
-                    if not await self.robot.start_homing():
-                        rosys.notify('homing: failed')
-                    else:
-                        rosys.notify('homing successful')
-                self.automator.start(try_axis_home())
+        # if e.modifiers.shift and e.action.keydown:
+        #     if e.key == '!':
+        #         async def try_axis_home():
+        #             if not await self.robot.start_homing():
+        #                 rosys.notify('homing: failed')
+        #             else:
+        #                 rosys.notify('homing successful')
+        #         self.automator.start(try_axis_home())
 
         if e.modifiers.shift and e.action.keydown:
             if e.key == 'W':
-                rosys.task_logger.create_task(self.robot.move_zaxis_to(self.robot.MAX_Z))
+                background_tasks.create(self.z_axis.move_to(self.z_axis.MAX_Z))
             if e.key == 'S':
-                rosys.task_logger.create_task(self.robot.move_zaxis_to(self.robot.MIN_Z))
+                background_tasks.create(self.z_axis.move_to(self.z_axis.MIN_Z))
         if e.modifiers.shift and e.action.keyup:
             if e.key.name in 'WS':
-                rosys.task_logger.create_task(self.robot.stop_zaxis())
+                background_tasks.create(self.z_axis.stop())
 
         if e.modifiers.shift and e.action.keydown:
             if e.key == 'A':
-                rosys.task_logger.create_task(self.robot.move_yaxis_to(self.robot.MAX_Y))
+                background_tasks.create(self.y_axis.move_to(self.y_axis.MAX_Y))
             if e.key == 'D':
-                rosys.task_logger.create_task(self.robot.move_yaxis_to(self.robot.MIN_Y))
+                background_tasks.create(self.y_axis.move_to(self.y_axis.MIN_Y))
         if e.modifiers.shift and e.action.keyup:
             if e.key.name in 'AD':
-                rosys.task_logger.create_task(self.robot.stop_yaxis())
+                background_tasks.create(self.y_axis.stop())
 
         if e.key == ' ' and e.action.keydown:
-            rosys.task_logger.create_task(self.robot.stop())
+            background_tasks.create(self.wheels.stop())
+            # TODO: stopp all background tasks when the operation is stopped
