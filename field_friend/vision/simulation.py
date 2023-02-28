ce9aec51e6081c568b535c7abe5d01a160cbf6f6
Miguel <angelom93@web.de>
2023-02-28 19:16:52 +0100

    fix for camera simulation

--- field_friend/vision/simulation.py
+++ field_friend/vision/simulation.py
@@ -5,7 +5,7 @@ import rosys
 def create_weedcam(usb_camera_provider: rosys.vision.UsbCameraProviderSimulation) \
         -> rosys.vision.UsbCamera:
     usb_camera_provider.cameras.clear()
-    camera = usb_camera_provider.create_calibrated('weedcam', color='#555',
+    camera = usb_camera_provider.create_calibrated('front_cam', color='#555',
                                                    x=0, y=0, z=0.3,
                                                    roll=np.deg2rad(360-150), pitch=np.deg2rad(0), yaw=np.deg2rad(-90))
     usb_camera_provider.add_camera(camera)
