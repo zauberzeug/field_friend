dc749c50b5c3a0573a5ba40baaa663534249c4d9
Miguel <angelom93@web.de>
2023-03-01 15:29:45 +0100

    ui fix

--- main.py
+++ main.py
@@ -15,13 +15,14 @@ def startup() -> None:
     async def main_page():
         ui.colors(primary='#6E93D6', secondary='#53B689', accent='#111B1E', positive='#53B689')
         interface.navigation_bar(system.field_friend)
-
-        with ui.row().classes('fit items-stretch justify-around').style('flex-wrap:nowrap'):
-            interface.operation(system.field_friend, system.steerer, system.automator, system.odometer,
-                                system.usb_camera_provider, system.plant_provider, system.puncher)
-            interface.camera(system.camera_selector, system.usb_camera_provider,
-                             system.automator, system.detector, system.puncher)
-        interface.development(system.field_friend, system.automator, system.puncher)
+        with ui.column().classes('w-full items-stretch'):
+            with ui.row().classes('items-stretch justify-items-stretch').style('flex-wrap:nowrap'):
+                interface.operation(system.field_friend, system.steerer, system.automator, system.odometer,
+                                    system.usb_camera_provider, system.plant_provider, system.puncher)
+                # interface.camera(system.camera_selector, system.usb_camera_provider,
+                #                  system.automator, system.detector, system.puncher)
+            with ui.row().classes('items-stretch justify-items-stretch'):
+                interface.development(system.field_friend, system.automator, system.puncher)
 
 
 app.on_startup(startup)
