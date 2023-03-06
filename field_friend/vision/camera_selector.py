f523e9d8104081763efaaf403e6732dba8027d29
Miguel <angelom93@web.de>
2023-03-06 08:33:12 +0100

    rewrite camera selcetor

--- field_friend/vision/camera_selector.py
+++ field_friend/vision/camera_selector.py
@@ -2,34 +2,29 @@ import logging
 
 import rosys
 
-FRONT_CAMERA_IDS = ['front_cam', 'usb-70090000.xusb-2.4.2']
-BOTTOM_CAMERA_IDS = ['bottom_cam', 'usb-70090000.xusb-2.4.1']
-
 
 class CameraSelector:
-    def __init__(self, camera_provider: rosys.vision.CameraProvider) -> None:
-        self.FRONT_CAMERA_SELECTED = rosys.event.Event()
-        '''A new front camera was selected'''
-        self.BOTTOM_CAMERA_SELECTED = rosys.event.Event()
-        '''A new bottom camera was selected'''
 
+    FRONT_CAMERA_IDS = ['front_cam', 'usb-70090000.xusb-2.4.2']
+    BOTTOM_CAMERA_IDS = ['bottom_cam', 'usb-70090000.xusb-2.4.1']
+
+    def __init__(self, camera_provider: rosys.vision.CameraProvider) -> None:
+        self.CAMERA_SELECTED = rosys.event.Event()
+        '''A camera was selected'''
+
+        self.camera_ids = {
+            'front cam': self.FRONT_CAMERA_IDS, 'bottom cam': self.BOTTOM_CAMERA_IDS
+        }
         self.camera_provider = camera_provider
-        self.camera: rosys.vision.Camera = None
+        self.cameras: dict[str, rosys.vision.Camera] = {}
         self.log = logging.getLogger('field_friend.camera_selector')
         camera_provider.CAMERA_ADDED.register(self.use_camera)
 
     def use_camera(self, camera: rosys.vision.Camera) -> None:
-        self.camera = camera
-        self.log.info(f'selected camera is {self.camera.id}')
-        if camera.id not in FRONT_CAMERA_IDS and camera.id not in BOTTOM_CAMERA_IDS:
-            self.log.warning(f'camera {camera.id} is not a known camera')
-            return
-        self.log.info(f'selected camera is {self.camera.id}')
-        if camera.id in FRONT_CAMERA_IDS:
-            self.log.info(f'camera {camera.id} is a front camera')
-            self.FRONT_CAMERA_SELECTED.emit(self.camera)
-            return
-        if camera.id in BOTTOM_CAMERA_IDS:
-            self.log.info(f'camera {camera.id} is a bottom camera')
-            self.BOTTOM_CAMERA_SELECTED.emit(self.camera)
-            return
+        for camera_type, camera_ids_list in self.camera_ids.items():
+            if camera.id in camera_ids_list:
+                self.log.info(f'camera {camera.id} is a {camera_type}')
+                self.cameras[camera_type] = camera
+                self.CAMERA_SELECTED.emit((camera_type, camera))
+                return
+        self.log.warning(f'camera {camera.id} is not a known camera')
