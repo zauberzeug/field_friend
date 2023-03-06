5f597ccc29c42c4445bcf5ee2f2be3315b2812d6
Miguel <angelom93@web.de>
2023-03-06 08:33:24 +0100

    small ui fixes

--- field_friend/interface/navigation_bar.py
+++ field_friend/interface/navigation_bar.py
@@ -10,15 +10,15 @@ def navigation_bar(field_friend: FieldFriend):
         ui.label('Zauberzeug Field Friend').classes('text-white uppercase text-weight-bold col-5 q-pl-md mt-1')
         with ui.row().classes('col-7 justify-end q-pr-md items-bottom'):
             with ui.row().bind_visibility_from(field_friend.e_stop, 'emergency_stop'):
-                ui.icon('report').classes('text-red')
+                ui.icon('report').classes('text-red').props('size=sm dense unelevated flat')
                 ui.label('emergency stop is pressed').classes('text-red mt-1')
             if isinstance(field_friend, FieldFriendHardware):
-                ui.icon('hardware').classes('text-white')
+                ui.icon('hardware').classes('text-white').props('size=sm dense unelevated flat')
                 ui.label('Hardware').classes('text-white mt-1')
             else:
-                ui.icon('computer').classes('text-white')
+                ui.icon('computer').classes('text-white').props('size=sm dense unelevated flat')
                 ui.label('Simulation').classes('text-white mt-1')
-            ui.icon('battery_full').classes('text-white')
+            ui.icon('battery_full').classes('text-white').props('size=sm dense unelevated flat')
             battery_status = ui.markdown().classes('text-white mt-1')
             with ui.row():
                 with ui.menu() as menu:
