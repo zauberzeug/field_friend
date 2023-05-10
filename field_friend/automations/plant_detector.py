b70d8be300551ef001f07e83786f4570d2889ea3
Falko Schindler <falko@zauberzeug.com>
2023-05-10 12:04:58 +0200

    let the plant provider transform detections into world coordinates

--- field_friend/automations/plant_detector.py
+++ field_friend/automations/plant_detector.py
@@ -17,10 +17,15 @@ class DetectorError(Exception):
 
 class PlantDetector:
 
-    def __init__(self, detector: rosys.vision.Detector, plant_provider: PlantProvider) -> None:
+    def __init__(self,
+                 detector: rosys.vision.Detector,
+                 plant_provider: PlantProvider,
+                 odometer: rosys.driving.Odometer,
+                 ) -> None:
         self.log = logging.getLogger('field_friend.plant_detection')
         self.detector = detector
         self.plant_provider = plant_provider
+        self.odometer = odometer
         self.weed_category_names: list[str] = WEED_CATEGORY_NAME
         self.crop_category_names: list[str] = CROP_CATEGORY_NAME
         self.minimum_weed_confidence: float = MINIMUM_WEED_CONFIDENCE
@@ -32,25 +37,28 @@ class PlantDetector:
         if camera.calibration is None:
             rosys.notify('camera has no calibration')
             raise DetectorError()
-        
+
         image = camera.latest_captured_image
         if image is None:
             self.log.info('no image found')
             raise DetectorError()
-        
+
         await self.detector.detect(image)
         if not image.detections:
             self.log.info('no detection found')
             raise DetectorError()
 
+        print(f'{len(image.detections.points)=}')
         for d in image.detections.points:
             if d.category_name in self.weed_category_names and d.confidence >= self.minimum_weed_confidence:
                 image_point = rosys.geometry.Point(x=d.cx, y=d.cy)
-                world_point = camera.calibration.project_from_image(image_point)
-                weed = Plant(position=world_point.projection(), type=d.category_name, detection_time=rosys.time())
+                floor_point = camera.calibration.project_from_image(image_point).projection()
+                world_point = self.odometer.prediction.transform(floor_point)
+                weed = Plant(position=world_point, type=d.category_name, detection_time=rosys.time())
                 self.plant_provider.add_weed(weed)
             elif d.category_name in self.crop_category_names and d.confidence >= self.minimum_crop_confidence:
                 image_point = rosys.geometry.Point(x=d.cx, y=d.cy)
-                world_point = camera.calibration.project_from_image(image_point)
-                crop = Plant(position=world_point.projection(), type=d.category_name, detection_time=rosys.time())
+                floor_point = camera.calibration.project_from_image(image_point).projection()
+                world_point = self.odometer.prediction.transform(floor_point)
+                crop = Plant(position=world_point, type=d.category_name, detection_time=rosys.time())
                 self.plant_provider.add_crop(crop)
