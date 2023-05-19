36b709eb5272c98350a858678f5b846b595bb6a4
Miguel <angelom93@web.de>
2023-05-19 10:32:10 +0200

    rewrite field_friend zaxis with ref and end

--- field_friend/hardware/z_axis.py
+++ field_friend/hardware/z_axis.py
@@ -6,25 +6,25 @@ from rosys.helpers import remove_indentation
 
 
 class ZAxis(rosys.hardware.Module, abc.ABC):
-    """The z axis module is a simple example for a representation of real or simulated robot hardware."""
-
-    Z_AXIS_MAX_SPEED: float = 80_000
-    MIN_Z: float = -0.197
-    MAX_Z: float = -0.003
-    STEPS_PER_MM: float = 1600
+    MAX_SPEED: float = 66_000
+    MIN_DEPTH: float = 0.00
+    MAX_DEPTH: float = 0.197
+    STEPS_PER_M: float = 1600 * 1000
+    REF_OFFSET: int = 0.0001
 
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
 
-        self.end_t: bool = False
-        self.end_b: bool = False
-        self.position: int = 0
+        self.steps: int = 0
         self.alarm: bool = False
         self.idle: bool = False
+
         self.is_referenced: bool = False
-        self.home_position: int = 0
-        self.drill_depth: float = self.MIN_Z
-        self.end_stops_active: bool = True
+        self.is_ref_enabled: bool = False
+        self.is_end_b_enabled: bool = False
+
+        self.ref_t: bool = False
+        self.end_b: bool = False
 
         rosys.on_shutdown(self.stop)
 
@@ -33,38 +33,51 @@ class ZAxis(rosys.hardware.Module, abc.ABC):
         pass
 
     @abc.abstractmethod
-    async def move_to(self, world_position: float, speed: float) -> float:
+    async def move_to(self, depth: float, speed: int) -> float:
         if not self.is_referenced:
-            rosys.notify('zaxis is not referenced, reference first')
-            return None
-        if self.end_b or self.end_t:
-            rosys.notify('zaxis is in end stops, remove to move')
-            return None
-        if speed > self.Z_AXIS_MAX_SPEED:
-            rosys.notify('zaxis speed is too high')
-            return None
-        if world_position < self.MIN_Z or world_position > self.MAX_Z:
-            rosys.notify('zaxis position is out of range')
-            return None
-        steps = self.depth_to_steps(world_position)
-        target_position = self.home_position + steps
-        return target_position
+            raise RuntimeError('zaxis is not referenced, reference first')
+        if speed > self.MAX_SPEED:
+            raise RuntimeError('zaxis speed is too high')
+        if not self.MIN_DEPTH - self.REF_OFFSET <= depth <= self.MAX_DEPTH:
+            raise RuntimeError('zaxis depth is out of range')
+
+    @abc.abstractmethod
+    async def move(self, speed: int) -> None:
+        pass
 
     @abc.abstractmethod
     async def try_reference(self) -> bool:
         return True
 
-    def depth_to_steps(self, depth: float) -> int:
-        steps = int((depth * 1000) * self.STEPS_PER_MM)
-        return steps
+    def compute_steps(self, depth: float) -> int:
+        """Compute the number of steps to move the z axis to the given depth.
 
-    def steps_to_depth(self, steps: int) -> float:
-        depth = (steps * (1 / self.STEPS_PER_MM)) / 1000
-        return depth
+        Depth is positive and steps are negative when moving down.
+        """
+        return int(-depth * self.STEPS_PER_M)
 
-    async def enable_end_stops(self, value: bool) -> None:
-        self.end_stops_active = value
-        self.log.info(f'end stops active = {value}')
+    def compute_depth(self, steps: int) -> float:
+        """Compute the depth of the z axis from the given number of steps.
+
+        Depth is positive and steps are negative when moving down.
+        """
+        return -steps / self.STEPS_PER_M
+
+    @property
+    def depth(self) -> float:
+        return self.compute_depth(self.steps)
+
+    async def enable_end_stop(self, value: bool) -> None:
+        pass
+
+    async def enable_ref_stop(self, value: bool) -> None:
+        pass
+
+    async def return_to_reference(self) -> bool:
+        try:
+            await self.move_to(0 - self.REF_OFFSET)
+        except RuntimeError as e:
+            rosys.notify(e, type='negative')
 
 
 class ZAxisHardware(ZAxis, rosys.hardware.ModuleHardware):
@@ -76,44 +89,64 @@ class ZAxisHardware(ZAxis, rosys.hardware.ModuleHardware):
                  step_pin: int = 5,
                  dir_pin: int = 4,
                  alarm_pin: int = 33,
-                 end_t_pin: int = 13,
+                 ref_t_pin: int = 13,
                  end_b_pin: int = 15,) -> None:
         self.name = name
         self.expander = expander
         lizard_code = remove_indentation(f'''
-            {name} = {expander.name}.StepperMotor({step_pin}, {dir_pin})
-            {name}_alarm = {expander.name}.Input({alarm_pin})
-            {name}_end_t = {expander.name}.Input({end_t_pin})
-            {name}_end_b = {expander.name}.Input({end_b_pin})
+            {name} = {expander.name + "." if expander else ""}StepperMotor({step_pin}, {dir_pin})
+            {name}_alarm = {expander.name + "." if expander else ""}Input({alarm_pin})
+            {name}_ref_t = {expander.name + "." if expander else ""}Input({ref_t_pin})
+            {name}_end_b = {expander.name + "." if expander else ""}Input({end_b_pin})
             bool {name}_is_referencing = false;
-            bool {name}_end_stops_active = true
-            when {name}_end_stops_active and {name}_is_referencing and {name}_end_t.level == 0 then
+            bool {name}_ref_t_enabled = true;
+            bool {name}_end_b_enabled = true;
+            when {name}_ref_t_enabled and {name}_is_referencing and {name}_ref_t.level == 0 then
                 {name}.stop();
-                {name}_end_stops_active = false;
+                {name}_ref_t_enabled = false;
             end
-            when !{name}_end_stops_active and {name}_is_referencing and {name}_end_t.level == 1 then 
+            when !{name}_ref_t_enabled and {name}_is_referencing and {name}_ref_t.level == 1 then 
                 {name}.stop(); 
-                {name}_end_stops_active = true;
+                {name}_ref_t_enabled = true;
             end
-            when !{name}_is_referencing and {name}_end_stops_active and {name}_end_t.level == 0 then {name}.stop(); end
-            when {name}_end_stops_active and {name}_end_b.level == 0 then {name}.stop(); end
+            when {name}_end_b_enabled and {name}_end_b.level == 0 then {name}.stop(); end
         ''')
-        core_message_fields = [f'{name}_end_t.level', f'{name}_end_b.level',
-                               f'{name}.idle', f'{name}.position', f'{name}_alarm.level']
+        core_message_fields = [
+            f'{name}_ref_t.level',
+            f'{name}_end_b.level',
+            f'{name}.idle',
+            f'{name}.position',
+            f'{name}_alarm.level',
+            f'{name}_ref_t_enabled',
+            f'{name}_end_b_enabled',
+        ]
         super().__init__(robot_brain=robot_brain, lizard_code=lizard_code, core_message_fields=core_message_fields)
 
     async def stop(self) -> None:
         await super().stop()
         await self.robot_brain.send(f'{self.name}.stop()')
 
-    async def move_to(self, world_position: float, speed: float = 80_000) -> None:
-        target_position = await super().move_to(world_position=world_position, speed=speed)
-        if target_position is None:
+    async def move_to(self, depth: float, speed: float = ZAxis.MAX_SPEED) -> None:
+        try:
+            await super().move_to(depth, speed)
+        except RuntimeError as e:
+            rosys.notify(e, type='negative')
             return
-        await self.robot_brain.send(f'{self.name}.position({target_position}, {speed}, 160000);')
+        steps = self.compute_steps(depth)
+
+        await self.robot_brain.send(f'{self.name}.position({steps}, {speed}, 66000);')
+        await rosys.sleep(0.2)
         if not await self.check_idle_or_alarm():
+            rosys.notify('z_axis fault detected', type='negative')
             return
-        self.log.info(f'zaxis moved to {world_position}')
+
+    async def move(self, speed: int) -> None:
+        try:
+            await super().move(speed)
+        except RuntimeError as e:
+            rosys.notify(e, type='negative')
+            return
+        await self.robot_brain.send(f'{self.name}.speed({speed}, 8000)')
 
     async def check_idle_or_alarm(self) -> bool:
         while not self.idle and not self.alarm:
@@ -127,49 +160,90 @@ class ZAxisHardware(ZAxis, rosys.hardware.ModuleHardware):
         if not await super().try_reference():
             return False
         try:
-            if not self.end_stops_active:
-                self.log.warning('end stops not activated')
-                return False
-            if self.end_t or self.end_b:
-                self.log.info('zaxis is in end stops, remove to reference')
-                return False
+            self.log.info('zaxis referencing...')
+
+            await self.robot_brain.send(f'{self.name}_is_referencing = true;')
+            await rosys.sleep(0.5)
+
+            # if in end b stop, diasble end b stop
+            if self.end_b:
+                await self.enable_end_stop(False)
+                await rosys.sleep(0.5)
+
+            # if in ref t stop, move out
+            if self.ref_t:
+                await self.enable_ref_stop(False)
+                await rosys.sleep(0.5)
+                await self.robot_brain.send(
+                    f'{self.name}.speed(-{self.MAX_SPEED/2});'
+                )
+                while self.ref_t:
+                    await rosys.sleep(0.2)
+                await self.stop()
+
+            # move to top ref
+            await self.robot_brain.send(f'{self.name}.speed({self.MAX_SPEED/2});')
+            while not self.ref_t:
+                await rosys.sleep(0.2)
+            await self.stop()
+
+            # move out of top ref
+            await self.robot_brain.send(f'{self.name}.speed(-{self.MAX_SPEED/2});')
+            while self.ref_t:
+                await rosys.sleep(0.2)
+            await self.stop()
+
+            # move slowly to top ref
+            await self.robot_brain.send(f'{self.name}.speed({self.MAX_SPEED/10});')
+            while not self.ref_t:
+                await rosys.sleep(0.2)
+            await self.stop()
+
+            # save position
+            await rosys.sleep(0.2)
             await self.robot_brain.send(
-                f'{self.name}_is_referencing = true;'
-                f'{self.name}.speed({self.Z_AXIS_MAX_SPEED/2});'
+                f'{self.name}.position = 0;'
             )
-            if not await self.check_idle_or_alarm():
-                return False
-            await self.robot_brain.send(f'{self.name}.speed(-{self.Z_AXIS_MAX_SPEED/2});')
-            if not await self.check_idle_or_alarm():
-                return False
-            await self.robot_brain.send(f'{self.name}.speed({self.Z_AXIS_MAX_SPEED/10});')
-            if not await self.check_idle_or_alarm():
-                return False
-            await self.robot_brain.send(f'{self.name}.speed(-{self.Z_AXIS_MAX_SPEED/10});')
-            if not await self.check_idle_or_alarm():
-                return False
-            await self.robot_brain.send(f'{self.name}_is_referencing = false;')
-            await rosys.sleep(0.1)
-            self.home_position = self.position
+            await self.robot_brain.send(
+                f'{self.name}_is_referencing = false;'
+            )
+            await rosys.sleep(0.5)
             self.is_referenced = True
-            await self.move_to(self.MAX_Z, speed=self.Z_AXIS_MAX_SPEED/2)
+
+            # drive to reference with offset
+            await self.return_to_reference()
+            await self.enable_end_stop(True)
+
             self.log.info('zaxis referenced')
             return True
+        except Exception as e:
+            self.log.error(f'zaxis reference failed: {e}')
+            return False
         finally:
             await self.stop()
 
-    async def enable_end_stops(self, value: bool) -> None:
+    async def enable_ref_stop(self, value: bool) -> None:
+        await super().enable_ref_stop(value)
+        self.log.info(f'zaxis ref t stop enabled: {value}')
+        await self.robot_brain.send(f'{self.name}_ref_t_enabled = {str(value).lower()};')
+
+    async def enable_end_stop(self, value: bool) -> None:
         await super().enable_end_stops(value)
-        await self.robot_brain.send(f'{self.name}_end_stops_active = {str(value).lower()};')
+        self.log.info(f'zaxis end b stop enabled: {value}')
+        await self.robot_brain.send(f'{self.name}_end_b_enabled = {str(value).lower()};')
 
     def handle_core_output(self, time: float, words: list[str]) -> None:
-        self.end_t = int(words.pop(0)) == 0
+        self.ref_t = int(words.pop(0)) == 0
         self.end_b = int(words.pop(0)) == 0
+        if self.end_b:
+            self.is_referenced = False
         self.idle = words.pop(0) == 'true'
-        self.position = int(words.pop(0))
+        self.steps = int(words.pop(0))
         self.alarm = int(words.pop(0)) == 0
         if self.alarm:
             self.is_referenced = False
+        self.is_ref_enabled = words.pop(0) == 'true'
+        self.is_end_b_enabled = words.pop(0) == 'true'
 
 
 class ZAxisSimulation(ZAxis, rosys.hardware.ModuleSimulation):
@@ -177,39 +251,43 @@ class ZAxisSimulation(ZAxis, rosys.hardware.ModuleSimulation):
 
     def __init__(self) -> None:
         super().__init__()
-        self.velocity: float = 0.0
-        self.target: Optional[float] = None
+        self.speed: int = 0
+        self.target_steps: Optional[int] = None
+        self.reference_steps: int = 0
 
     async def stop(self) -> None:
         await super().stop()
-        self.velocity = 0.0
-        self.target = None
+        self.speed = 0
+        self.target_steps = None
 
-    async def move_to(self, world_position: float, speed: float = 16_000) -> None:
-        target_position = await super().move_to(world_position=world_position, speed=speed)
-        if target_position is None:
+    async def move_to(self, depth: float, speed: int = ZAxis.MAX_SPEED) -> None:
+        try:
+            await super().move_to(depth, speed)
+        except RuntimeError as e:
+            rosys.notify(e, type='negative')
             return
-        self.target = target_position
-        if self.target > self.position:
-            self.velocity = speed
-        if self.target < self.position:
-            self.velocity = -speed
-        while self.target is not None:
+        self.target_steps = self.compute_steps(depth)
+        self.speed = speed if self.target_steps > self.steps else -speed
+        while self.target_steps is not None:
             await rosys.sleep(0.2)
+
+    async def move(self, speed: int) -> None:
+        await super().move(speed)
+        self.speed = speed
 
     async def try_reference(self) -> bool:
         if not await super().try_reference():
             return False
-        self.position = 0
-        self.home_position = 0
+        self.steps = 0
+        self.reference_steps = 0
         self.is_referenced = True
         return True
 
     async def step(self, dt: float) -> None:
-        await super().step(dt)
-        self.position += int(dt * self.velocity)
-        if self.target is not None:
-            if (self.velocity > 0) == (self.position > self.target):
-                self.position = self.target
-                self.target = None
-                self.velocity = 0
+        self.steps += int(dt * self.speed)
+        self.is_reference_active = self.steps >= self.reference_steps
+        self.idle = self.speed == 0
+        if self.target_steps is not None and (self.speed > 0) == (self.steps > self.target_steps):
+            self.steps = self.target_steps
+            self.target_steps = None
+            self.speed = 0
