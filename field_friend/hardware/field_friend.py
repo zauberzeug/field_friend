b1f4c3f61e37893a24d1003faee2ac7237316fbc
Miguel <angelom93@web.de>
2023-07-03 13:20:44 +0200

    setup for ff3

--- field_friend/hardware/field_friend.py
+++ field_friend/hardware/field_friend.py
@@ -10,8 +10,10 @@ from .z_axis import ZAxis, ZAxisHardware, ZAxisSimulation
 
 class FieldFriend(rosys.hardware.Robot):
     MOTOR_GEAR_RATIO = 12.52
-    WHEEL_DIAMETER = 0.23
+    WHEEL_DIAMETER = 0.041 * 17 / np.pi
     M_PER_TICK = WHEEL_DIAMETER * np.pi / MOTOR_GEAR_RATIO
+    WORK_X = 0.118
+    DRILL_RADIUS = 0.025
 
     def __init__(
             self, *,
@@ -39,6 +41,15 @@ class FieldFriend(rosys.hardware.Robot):
             await self.z_axis.stop()
         # TODO: stop other modules
 
+    def can_reach(self, local_point: rosys.geometry.Point) -> bool:
+        """Check if the given point is reachable by the tool.
+
+        The point is given in local coordinates, i.e. the origin is the center of the tool.
+        """
+        return self.WORK_X - self.DRILL_RADIUS <= local_point.x <= self.WORK_X + self.DRILL_RADIUS \
+            and self.y_axis.MIN_POSITION <= local_point.y <= self.y_axis.MAX_POSITION \
+
+
 
 class FieldFriendHardware(FieldFriend, rosys.hardware.RobotHardware):
 
@@ -47,6 +58,7 @@ class FieldFriendHardware(FieldFriend, rosys.hardware.RobotHardware):
         robot_brain = rosys.hardware.RobotBrain(communication)
         if communication.device_path == '/dev/ttyTHS0':
             robot_brain.lizard_firmware.flash_params = ['xavier']
+        bluetooth = rosys.hardware.BluetoothHardware(robot_brain, name='Fieldfriend')
         can = rosys.hardware.CanHardware(robot_brain)
         wheels = rosys.hardware.WheelsHardware(robot_brain,
                                                can=can,
@@ -54,22 +66,35 @@ class FieldFriendHardware(FieldFriend, rosys.hardware.RobotHardware):
                                                right_can_address=0x100,
                                                m_per_tick=self.M_PER_TICK,
                                                width=0.47,
-                                               is_left_reversed=True)
-
+                                               is_right_reversed=True)
         serial = rosys.hardware.SerialHardware(robot_brain)
         expander = rosys.hardware.ExpanderHardware(robot_brain, serial=serial)
         if with_yaxis:
-            y_axis = YAxisHardware(robot_brain, expander=expander)
+            y_axis = YAxisHardware(robot_brain,
+                                   expander=expander,
+                                   step_pin=5,
+                                   dir_pin=4,
+                                   alarm_pin=13,
+                                   end_stops_on_expander=True,
+                                   end_l_pin=21,
+                                   end_r_pin=35)
         else:
             y_axis = None
         if with_zaxis:
-            z_axis = ZAxisHardware(robot_brain, expander=expander)
+            z_axis = ZAxisHardware(robot_brain,
+                                   expander=expander,
+                                   alarm_pin=33,
+                                   motor_on_expander=True,
+                                   end_stops_on_expander=True,
+                                   ref_t_pin=25,
+                                   end_b_pin=22)
         else:
             z_axis = None
         bms = rosys.hardware.BmsHardware(robot_brain, expander=expander, rx_pin=26, tx_pin=27, num=2)
         estop = rosys.hardware.EStopHardware(robot_brain, pins={'1': 34, '2': 35})
+        self.battery_control = rosys.hardware.BatteryControlHardware(robot_brain, expander=expander)
         safety = SafetyHardware(robot_brain, estop=estop, wheels=wheels, y_axis=y_axis, z_axis=z_axis)
-        modules = [can, wheels, serial, expander, y_axis, z_axis, bms, estop, safety]
+        modules = [bluetooth, can, wheels, serial, expander, y_axis, z_axis, bms, estop, self.battery_control, safety]
         active_modules = [module for module in modules if module is not None]
 
         super().__init__(wheels=wheels,
@@ -96,7 +121,7 @@ class FieldFriendSimulation(FieldFriend, rosys.hardware.RobotSimulation):
             z_axis = ZAxisSimulation()
         else:
             z_axis = None
-        safety = SafetySimulation(wheels, estop, y_axis=y_axis, z_axis=z_axis)
+        safety = SafetySimulation(wheels=wheels, estop=estop, y_axis=y_axis, z_axis=z_axis)
         super().__init__(wheels=wheels,
                          y_axis=y_axis,
                          z_axis=z_axis,
--- system.py
+++ system.py
@@ -2,8 +2,9 @@ import os
 
 import rosys
 
-from field_friend.automations import PlantProvider, Puncher, Weeding
+from field_friend.automations import PlantDetector, PlantProvider, Puncher, Weeding
 from field_friend.hardware import FieldFriendHardware, FieldFriendSimulation
+from field_friend.navigation import GnssHardware, GnssSimulation
 from field_friend.vision import CameraSelector
 from field_friend.vision.simulation import create_weedcam
 
@@ -13,31 +14,74 @@ class System:
         rosys.hardware.SerialCommunication.search_paths.insert(0, '/dev/ttyTHS0')
         self.is_real = rosys.hardware.SerialCommunication.is_possible()
         self.camera_selector = CameraSelector()
+        self.camera_selector.camera_ids = {
+            'bottom cam': self.camera_selector.BOTTOM_CAMERA_IDS,
+        }
         if self.is_real:
             self.field_friend = FieldFriendHardware()
             self.usb_camera_provider = rosys.vision.UsbCameraProviderHardware()
             self.detector = rosys.vision.DetectorHardware(port=8004)
+            self.gnss = GnssHardware()
         else:
 
             self.field_friend = FieldFriendSimulation()
             self.usb_camera_provider = rosys.vision.UsbCameraProviderSimulation()
             self.detector = rosys.vision.DetectorSimulation(self.usb_camera_provider)
+            self.gnss = GnssSimulation(self.field_friend.wheels)
         self.usb_camera_provider.CAMERA_ADDED.register(self.camera_selector.use_camera)
         self.plant_provider = PlantProvider()
         self.steerer = rosys.driving.Steerer(self.field_friend.wheels, speed_scaling=0.2)
         self.odometer = rosys.driving.Odometer(self.field_friend.wheels)
+        self.gnss.ROBOT_LOCATED.register(self.odometer.handle_detection)
         self.driver = rosys.driving.Driver(self.field_friend.wheels, self.odometer)
-        self.driver.parameters.linear_speed_limit = 0.2
-        self.driver.parameters.angular_speed_limit = 0.5
+        self.driver.parameters.linear_speed_limit = 0.4
+        self.driver.parameters.angular_speed_limit = 0.4
         self.driver.parameters.can_drive_backwards = False
+        self.driver.parameters.minimum_turning_radius = 1.0
         self.automator = rosys.automation.Automator(self.steerer, on_interrupt=self.field_friend.stop)
         self.puncher = Puncher(self.field_friend, self.driver)
+        self.plant_detector = PlantDetector(self.detector, self.plant_provider, self.odometer)
         self.weeding = Weeding(self.field_friend, self.driver, self.detector,
-                               self.camera_selector, self.plant_provider, self.puncher)
+                               self.camera_selector, self.plant_provider, self.puncher, self.plant_detector)
         self.automator.default_automation = self.weeding.start
 
-        if not self.is_real:
-            rosys.on_startup(lambda: create_weedcam('front_cam', self.usb_camera_provider))
+        if self.is_real:
+            rosys.automation.app_controls(self.field_friend.robot_brain, self.automator)
+
+            def configure_cameras() -> None:
+                for camera in self.usb_camera_provider.cameras.values():
+                    width, height, xoffset, yoffset = 1920, 1080, 420, 150
+                    camera.resolution = rosys.vision.ImageSize(width=width, height=height)
+                    camera.crop = rosys.geometry.Rectangle(
+                        x=xoffset, y=0, width=width - (2 * xoffset + 150),
+                        height=height - yoffset)
+                    # camera.crop = rosys.geometry.Rectangle(
+                    #     x=0, y=0, width=width,
+                    #     height=height)
+                    camera.auto_exposure = True
+            rosys.on_repeat(configure_cameras, 1.0)
+
+            self.was_charging = False
+
+            def check_if_charging():
+                if self.field_friend.bms.state.is_charging:
+                    self.was_charging = True
+                    return
+                if not self.field_friend.bms.state.is_charging and self.was_charging:
+                    self.automator.start(self.field_friend.battery_control.release_battery_relais())
+                    self.was_charging = False
+
+            def relase_relais_on_startup():
+                self.automator.start(self.field_friend.battery_control.release_battery_relais())
+            rosys.on_repeat(check_if_charging, 0.5)
+            rosys.on_startup(relase_relais_on_startup)
+        else:
+            rosys.on_startup(lambda: create_weedcam('bottom_cam', self.usb_camera_provider))
+
+        def stop():
+            if self.automator.is_running:
+                self.automator.stop(because='emergency stop triggered')
+        self.field_friend.estop.ESTOP_TRIGGERED.register(stop)
 
     def restart(self) -> None:
         os.utime('main.py')
