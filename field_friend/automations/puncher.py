074efe89f87f9e967a22d2ded8bb3e2045f5a438
Miguel <angelom93@web.de>
2023-07-03 13:21:09 +0200

    changed puncher logic

--- field_friend/automations/puncher.py
+++ field_friend/automations/puncher.py
@@ -1,3 +1,5 @@
+import logging
+
 import rosys
 from rosys.driving import Driver
 from rosys.geometry import Point
@@ -9,47 +11,56 @@ class Puncher:
     def __init__(self, field_friend: FieldFriend, driver: Driver) -> None:
         self.field_friend = field_friend
         self.driver = driver
+        self.log = logging.getLogger('field_friend.puncher')
 
-    async def home(self) -> None:
+    async def try_home(self) -> bool:
         try:
+            if self.field_friend.estop.active:
+                rosys.notify('Estop active, relaese first', 'error')
+                return False
             if not await self.field_friend.y_axis.try_reference():
                 return False
+            await rosys.sleep(0.2)
             if not await self.field_friend.z_axis.try_reference():
                 return False
+            return True
         except Exception as e:
             raise Exception('homing failed') from e
         finally:
-            self.field_friend.y_axis.stop()
-            self.field_friend.z_axis.stop()
+            await self.field_friend.y_axis.stop()
+            await self.field_friend.z_axis.stop()
 
-    async def drive_to_punch(self, target_distance_x: float) -> None:
-        min_drive_distance = 0.1
-        axis_distance = max(target_distance_x - self.field_friend.y_axis.AXIS_OFFSET_X, min_drive_distance)
-        target_point = Point(x=axis_distance, y=0)
-        await self.driver.drive_to(target_point)
-        await rosys.sleep(0.2)
-        await self.field_friend.wheels.stop()
+    async def drive_to_punch(self, local_target_x: float) -> None:
+        self.log.info(f'Driving to punch at {local_target_x}...')
+        if local_target_x < self.driver.odometer.prediction.x + self.field_friend.WORK_X:
+            self.log.info('Target is behind')
+            return
+        axis_distance = local_target_x - self.field_friend.WORK_X
+        local_target = Point(x=axis_distance, y=0)
+        world_target = self.driver.odometer.prediction.transform(local_target)
+        await self.driver.drive_to(world_target)
 
-    async def punch(self, y: float, depth: float = None, speed: float = None) -> None:
+    async def punch(self, y: float, depth: float) -> None:
         try:
             if not self.field_friend.y_axis.is_referenced or not self.field_friend.z_axis.is_referenced:
                 rosys.notify('axis are not referenced')
                 return
-            if speed == None:
-                speed = self.field_friend.z_axis.Z_AXIS_MAX_SPEED
-                speed = self.field_friend.y_axis.Y_AXIS_MAX_SPEED
-            await self.field_friend.y_axis.move_to(y, speed)
-            if depth == None:
-                depth = self.field_friend.z_axis.drill_depth
-            await self.field_friend.z_axis.move_to(depth, speed)
-            await self.field_friend.z_axis.move_to(self.field_friend.z_axis.MAX_Z, speed)
+            await self.field_friend.y_axis.move_to(y)
+            await self.field_friend.z_axis.move_to(depth)
+            await self.field_friend.z_axis.return_to_reference()
         except Exception as e:
             raise Exception('punching failed') from e
         finally:
-            self.field_friend.y_axis.stop()
-            self.field_friend.z_axis.stop()
+            await self.field_friend.y_axis.stop()
+            await self.field_friend.z_axis.stop()
 
-    async def drive_and_punch(self, x: float, y: float, depth: float = None, speed: float = None) -> None:
+    async def clear_view(self) -> None:
+        self.log.info('Clearing view...')
+        y = self.field_friend.y_axis.MIN_POSITION if self.field_friend.y_axis.position <= 0 else self.field_friend.y_axis.MAX_POSITION
+        await self.field_friend.y_axis.move_to(y)
+        await self.field_friend.y_axis.stop()
+
+    async def drive_and_punch(self, x: float, y: float, depth: float = 0.05) -> None:
         await self.drive_to_punch(x)
-        await self.punch(y, depth, speed)
-        await self.field_friend.y_axis.move_to(self.field_friend.y_axis.MAX_Y, speed)
+        await self.punch(y, depth)
+        await self.clear_view()
