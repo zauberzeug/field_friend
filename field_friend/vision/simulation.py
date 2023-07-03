28663030444533694dfabecb27ad57bb3f8ac3b8
Miguel <angelom93@web.de>
2023-07-03 13:16:54 +0200

    fixed camera simulation

--- field_friend/vision/simulation.py
+++ field_friend/vision/simulation.py
@@ -6,7 +6,7 @@ def create_weedcam(name: str, usb_camera_provider: rosys.vision.UsbCameraProvide
         -> rosys.vision.UsbCamera:
     usb_camera_provider.cameras.clear()
     camera = usb_camera_provider.create_calibrated(f'{name}', color='#555',
-                                                   x=0, y=0, z=0.3,
-                                                   roll=np.deg2rad(360-150), pitch=np.deg2rad(0), yaw=np.deg2rad(-90))
+                                                   x=0.4, z=0.4,
+                                                   roll=np.deg2rad(360-150), pitch=np.deg2rad(0), yaw=np.deg2rad(90))
     usb_camera_provider.add_camera(camera)
     return camera
