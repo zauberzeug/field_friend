68a9172c178a50eadae10a07072fe56b4e6869b6
Miguel <angelom93@web.de>
2023-07-03 13:20:56 +0200

    integrated new interface

--- main.py
+++ main.py
@@ -6,24 +6,32 @@ from field_friend import interface
 from system import System
 
 log = log.configure()
+app.add_static_files('/assets', 'assets')
 
 
 def startup() -> None:
     system = System()
 
     @ui.page('/')
-    async def main_page():
+    def page() -> None:
         ui.colors(primary='#6E93D6', secondary='#53B689', accent='#111B1E', positive='#53B689')
-        interface.navigation_bar(system.field_friend)
+        status_drawer = interface.status_drawer(system.field_friend, system.gnss, system.odometer)
+        interface.header_bar(system, status_drawer)
+        interface.system_bar()
         with ui.column().classes('w-full items-stretch'):
             with ui.row().classes('items-stretch justify-items-stretch').style('flex-wrap:nowrap'):
-                interface.operation(system.field_friend, system.steerer, system.automator, system.odometer,
-                                    system.usb_camera_provider, system.plant_provider, system.puncher)
+                interface.operation(
+                    system.field_friend, system.steerer, system.automator, system.odometer, system.usb_camera_provider,
+                    system.plant_provider, system.plant_detector, system.puncher, system.weeding)
                 interface.cameras(system.camera_selector, system.usb_camera_provider,
                                   system.automator, system.detector, system.puncher)
             with ui.row().classes('items-stretch justify-items-stretch'):
-                interface.development(system.field_friend, system.automator, system.puncher)
+                interface.development(system.field_friend)
                 interface.axis_control(system.field_friend, system.automator, system.puncher)
+
+    @app.get('/status')
+    def status():
+        return {'status': 'ok'}
 
 
 app.on_startup(startup)
