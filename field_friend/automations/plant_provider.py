a71b1a81942bdb9dce5af7a66c9fb78fa05dd281
Miguel <angelom93@web.de>
2023-07-11 16:22:56 +0200

    added vents for plant adding

--- field_friend/automations/plant_provider.py
+++ field_friend/automations/plant_provider.py
@@ -12,6 +12,12 @@ class PlantProvider:
         self.PLANTS_CHANGED = rosys.event.Event()
         """The collection of plants has changed."""
 
+        self.ADDED_NEW_WEED = rosys.event.Event()
+        """A new weed has been added."""
+
+        self.ADDED_NEW_BEET = rosys.event.Event()
+        """A new beet has been added."""
+
         rosys.on_repeat(self.prune, 10.0)
 
     def prune(self, max_age: float = 10 * 60.0) -> None:
@@ -20,8 +26,12 @@ class PlantProvider:
         self.PLANTS_CHANGED.emit()
 
     def add_weed(self, weed: Plant) -> None:
+        for w in self.weeds:
+            if w.position.distance(weed.position) < 0.02 and w.type == weed.type:
+                return
         self.weeds.append(weed)
         self.PLANTS_CHANGED.emit()
+        self.ADDED_NEW_WEED.emit()
 
     def remove_weed(self, weed_id: str) -> None:
         self.weeds[:] = [weed for weed in self.weeds if weed.id != weed_id]
@@ -32,8 +42,12 @@ class PlantProvider:
         self.PLANTS_CHANGED.emit()
 
     def add_crop(self, crop: Plant) -> None:
+        for c in self.crops:
+            if c.position.distance(crop.position) < 0.03 and c.type == crop.type:
+                return
         self.crops.append(crop)
         self.PLANTS_CHANGED.emit()
+        self.ADDED_NEW_BEET.emit()
 
     def remove_crop(self, crop: Plant) -> None:
         self.crops[:] = [c for c in self.crops if c.id != crop.id]
