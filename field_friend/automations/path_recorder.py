c53841d184d89443177adc631011a18cc907314b
Miguel <angelom93@web.de>
2023-07-21 09:18:40 +0200

    log less when recording path

--- field_friend/automations/path_recorder.py
+++ field_friend/automations/path_recorder.py
@@ -17,6 +17,9 @@ class PathRecorder:
         self.PATHS_CHANGED = rosys.event.Event()
         """The dict of paths has changed."""
 
+        self.PATH_DRIVING_STARTED = rosys.event.Event()
+        """A path is being driven (argument: list of PathSegments)."""
+
         self.paths: dict[str, list[rosys.driving.PathSegment]] = {}
         self.current_path_recording: str = ''
         self.state: Literal['recording', 'driving', 'idle'] = 'idle'
@@ -64,14 +67,16 @@ class PathRecorder:
                 last_pose = new_pose
             else:
                 self.log.info('not recording because no movement detected')
-            await rosys.sleep(1.5)
+            await rosys.sleep(10)
+        new_pose = self.driver.odometer.prediction
+        splines.append(Spline.from_poses(last_pose, new_pose))
         self.paths[name] = [PathSegment(spline=spline) for spline in splines]
         self.log.info(f'path {name} recorded with {self.paths[name] if self.paths[name] else "NO"} segments')
         self.invalidate()
 
     async def drive_path(self, name: str) -> None:
         self.log.info(f'driving path {name}')
-        self.driver.parameters.can_drive_backwards = True
+        # self.driver.parameters.can_drive_backwards = True
         # if self.gnss.device is None:
         #     self.log.warning('not driving because no GNSS device found')
         #     return
@@ -87,6 +92,7 @@ class PathRecorder:
             self.log.warning(f'path {name} is empty')
             return
         self.log.info(f'path: {path}')
+        self.PATH_DRIVING_STARTED.emit(path)
         # await self.driver.drive_to(path[0].spline.start)
         await self.driver.drive_path(path)
         self.state = 'idle'
