c0563b57d81971f9ce3d1810aaa48a91765aad73
Miguel <angelom93@web.de>
2023-07-25 14:42:07 +0200

    remove old hardware files

--- field_friend/old_hardware/__init__.py
+++
@@ -1,6 +0,0 @@
-from ..vision import simulation
-from ..vision.camera_selector import CameraSelector
-from .battery import Battery
-from .robot import Robot
-from .robot_hardware import RobotHardware
-from .robot_simulation import RobotSimulation
