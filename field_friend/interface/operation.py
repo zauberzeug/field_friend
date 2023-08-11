c6968d9e0b162610a5021b4f1169b545d219ea0e
Miguel <angelom93@web.de>
2023-08-11 15:21:34 +0200

    improve bidning of operation control

--- field_friend/interface/operation.py
+++ field_friend/interface/operation.py
@@ -3,13 +3,14 @@ import logging
 from typing import Callable
 
 from nicegui import events, ui
-from rosys.automation import Automator, automation_controls
+from rosys.automation import Automator
 from rosys.driving import Driver, Odometer, Steerer, driver_object, joystick
 from rosys.vision import CameraProvider
 
 from ..automations import Mowing, Puncher, Weeding, plant_detector, plant_provider
 from ..hardware import FieldFriend
 from ..navigation import FieldProvider, PathProvider
+from .automation_controls import automation_controls
 from .field_object import field_object
 from .key_controls import KeyControls
 from .plant_object import plant_objects
@@ -44,7 +45,6 @@ class operation:
         self.field_provider = field_provider
         self.mowing = mowing
         self.log = logging.getLogger('field_friend.operation')
-        self.field_provider.FIELDS_CHANGED.register(self.show_field_selection.refresh)
 
         with ui.card().tight():
             self.scene_look = False
@@ -81,16 +81,48 @@ class operation:
                         ui.number('speed', format='%.0f', max=4, min=1).props('dense outlined').classes(
                             'w-24 mr-4').bind_value(key_controls, 'speed').tooltip('Set the speed of the robot (1-4)')
                     with ui.row():
-                        def change_default_automation() -> None:
-                            automator.default_automation = automations[automations_toggle.value]
                         automations_toggle = ui.toggle(
                             [key for key in automations.keys()],
-                            value='weeding', on_change=change_default_automation)
-                        automation_controls(automator)
+                            value='weeding').bind_value(
+                            automator, 'default_automation', forward=lambda key: automations[key],
+                            backward=lambda automation: next(
+                                key for key, value in automations.items() if value == automation))
+                        with ui.dialog() as dialog, ui.card():
+                            ui.label('Do you want to continue the old mowing automation?')
+                            with ui.row():
+                                ui.button('Yes', on_click=lambda: dialog.submit('Yes'))
+                                ui.button('No', on_click=lambda: dialog.submit('No'))
+                                ui.button('Cancel', on_click=lambda: dialog.submit('Cancel'))
+
+                        async def ensure_start() -> bool:
+                            self.log.info('Ensuring start of automation')
+                            if not automations_toggle.value == 'mowing' or mowing.current_path == None:
+                                return True
+                            result = await dialog
+                            if result == 'Yes':
+                                self.mowing.continue_mowing = True
+                            elif result == 'No':
+                                self.mowing.continue_mowing = False
+                            elif result == 'Cancel':
+                                return False
+                            return True
+                        automation_controls(automator, can_start=ensure_start)
 
                     with ui.column().bind_visibility_from(automations_toggle, 'value', value='mowing'):
                         with ui.row():
-                            self.show_field_selection()
+                            @ ui.refreshable
+                            def show_field_selection() -> None:
+
+                                def set_field() -> None:
+                                    for field in self.field_provider.fields:
+                                        if field.name == self.field_selection.value:
+                                            self.mowing.field = field
+
+                                self.field_selection = ui.select(
+                                    [field.name for field in self.field_provider.fields],
+                                    with_input=True, on_change=set_field).tooltip('Select the field to mow')
+                            show_field_selection()
+                            self.field_provider.FIELDS_CHANGED.register(show_field_selection.refresh)
                             ui.number('padding', value=0.5, step=0.1, min=0.0, format='%.1f').props('dense outlined suffix=m').classes(
                                 'w-24').bind_value(mowing, 'padding').tooltip('Set the padding for the mowing automation')
                             ui.number('lane distance', value=0.5, step=0.1, min=0.0, format='%.1f').props('dense outlined suffix=m').classes(
@@ -103,22 +135,11 @@ class operation:
                         else:
                             ui.label('This Field Friend has no weeding tool available')
             with ui.row().classes('m-4'):
-                ui.button('emergency stop', on_click=field_friend.estop.software_emergency_stop).props('color=red').classes(
-                    'py-3 px-6 text-lg').bind_visibility_from(field_friend.estop, 'en3_active', value=False)
-                ui.button('emergency reset', on_click=field_friend.estop.release_en3).props(
-                    'color=red-700 outline').classes('py-3 px-6 text-lg').bind_visibility_from(field_friend.estop, 'en3_active')
+                ui.button('emergency stop', on_click=lambda: field_friend.estop.set_soft_estop(True)).props('color=red').classes(
+                    'py-3 px-6 text-lg').bind_visibility_from(field_friend.estop, 'is_soft_estop_active', value=False)
+                ui.button('emergency reset', on_click=lambda: field_friend.estop.set_soft_estop(False)).props(
+                    'color=red-700 outline').classes('py-3 px-6 text-lg').bind_visibility_from(field_friend.estop,
+                                                                                               'is_soft_estop_active', value=True)
                 ui.checkbox(
                     'Space bar emergency stop').tooltip(
                     'Enable or disable the emergency stop on space bar').bind_value(key_controls, 'estop_on_space')
-
-    @ui.refreshable
-    def show_field_selection(self) -> None:
-
-        def set_field() -> None:
-            for field in self.field_provider.fields:
-                if field.name == self.field_selection.value:
-                    self.mowing.field = field
-
-        self.field_selection = ui.select(
-            [field.name for field in self.field_provider.fields],
-            with_input=True, on_change=set_field).tooltip('Select the field to mow')
