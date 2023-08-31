dbc837adbf063c0770a6838260caa604c73bd7ec
Miguel <angelom93@web.de>
2023-08-31 15:55:38 +0200

    added row points visualisation

--- field_friend/interface/field_object.py
+++ field_friend/interface/field_object.py
@@ -1,8 +1,8 @@
 import numpy as np
-from nicegui.elements.scene_objects import Curve, Extrusion, Group
+from nicegui.elements.scene_objects import Curve, Extrusion, Group, Sphere
 from rosys.geometry import Spline
 
-from ..navigation import FieldProvider
+from ..automations import FieldProvider
 
 
 class field_object(Group):
@@ -26,7 +26,7 @@ class field_object(Group):
                 else:
                     outline = [[point.x, point.y] for point in field.outline]
                 with self.scene:
-                    Extrusion(outline, 0.5, wireframe=True).with_name(f'field_{field.id}').material('#63462D')
+                    Extrusion(outline, 0.5, wireframe=True).with_name(f'field_{field.id}').material('black')
 
                 for obstacle in field.obstacles:
                     outline = [[point.x, point.y] for point in obstacle.points]
@@ -34,8 +34,7 @@ class field_object(Group):
 
                 for row in field.rows:
                     if len(row.points) == 1:
-                        outline = [[row.points[0].x + 0.05 * np.cos(phi), row.points[0].y + 0.05 * np.sin(phi)]
-                                   for phi in np.linspace(0, 2 * np.pi, 16, endpoint=False)]
+                        continue
                     else:
                         for i in range(len(row.points) - 1):
                             spline = Spline.from_points(row.points[i], row.points[i + 1])
@@ -44,4 +43,13 @@ class field_object(Group):
                                 [spline.control1.x, spline.control1.y, 0],
                                 [spline.control2.x, spline.control2.y, 0],
                                 [spline.end.x, spline.end.y, 0],
-                            ).material('green').with_name(f'row_{row.id}_{i}')
+                            ).material('#6c541e').with_name(f'row_{row.id}_{i}')
+
+                    for point in row.points:
+                        with self.scene:
+                            Sphere(0.07).move(x=point.x, y=point.y, z=0.01).material(
+                                '#ff8800').with_name(f'row_{row.id}_point')
+                    for crop in row.crops:
+                        with self.scene:
+                            Sphere(0.05).move(x=crop.position.x, y=crop.position.y,
+                                              z=0.01).material('green').with_name(f'row_{row.id}_crop')
