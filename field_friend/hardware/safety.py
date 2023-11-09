df33a5df9c92465e56e3a748d32ce3c4533f5930
Miguel <angelom93@web.de>
2023-11-09 23:52:15 +0100

    updated safety module with tornado

--- field_friend/hardware/safety.py
+++ field_friend/hardware/safety.py
@@ -7,7 +7,9 @@ from .chain_axis import ChainAxis, ChainAxisHardware, ChainAxisSimulation
 from .flashlight import Flashlight, FlashlightHardware, FlashlightSimulation
 from .flashlight_pwm import FlashlightPWM, FlashlightPWMHardware, FlashlightPWMSimulation
 from .flashlight_v2 import FlashlightHardwareV2, FlashlightSimulationV2, FlashlightV2
+from .tornado import Tornado, TornadoHardware, TornadoSimulation
 from .y_axis import YAxis, YAxisHardware, YAxisSimulation
+from .y_axis_tornado import YAxisHardwareTornado, YAxisSimulationTornado, YAxisTornado
 from .z_axis import ZAxis, ZAxisHardware, ZAxisSimulation
 from .z_axis_v2 import ZAxisHardwareV2, ZAxisSimulationV2, ZAxisV2
 
@@ -18,8 +20,8 @@ class Safety(rosys.hardware.Module, abc.ABC):
     def __init__(self, *,
                  wheels: rosys.hardware.Wheels,
                  estop: rosys.hardware.EStop,
-                 y_axis: Union[YAxis, ChainAxis, None] = None,
-                 z_axis: Union[ZAxis, ZAxisV2, None] = None,
+                 y_axis: Union[YAxis, ChainAxis, YAxisTornado, None] = None,
+                 z_axis: Union[ZAxis, ZAxisV2, Tornado, None] = None,
                  flashlight: Union[Flashlight, FlashlightV2, FlashlightPWM, None] = None,
                  **kwargs) -> None:
         super().__init__(**kwargs)
@@ -34,10 +36,10 @@ class SafetyHardware(Safety, rosys.hardware.ModuleHardware):
     """This module implements safety hardware."""
 
     def __init__(self, robot_brain: rosys.hardware.RobotBrain, *,
-                 wheels: rosys.hardware.Wheels,
+                 wheels: rosys.hardware.WheelsHardware,
                  estop: rosys.hardware.EStop,
-                 y_axis: Union[YAxisHardware, ChainAxisHardware, None] = None,
-                 z_axis: Union[ZAxisHardware, ZAxisHardwareV2, None] = None,
+                 y_axis: Union[YAxisHardware, ChainAxisHardware, YAxisHardwareTornado, None] = None,
+                 z_axis: Union[ZAxisHardware, ZAxisHardwareV2, TornadoHardware, None] = None,
                  flashlight: Union[FlashlightHardware, FlashlightHardwareV2, FlashlightPWMHardware, None],
                  ) -> None:
 
@@ -45,7 +47,11 @@ class SafetyHardware(Safety, rosys.hardware.ModuleHardware):
         if y_axis is not None:
             lizard_code += f' {y_axis.name}.stop();'
         if z_axis is not None:
-            lizard_code += f' {z_axis.name}.stop();'
+            if not isinstance(z_axis, TornadoHardware):
+                lizard_code += f' {z_axis.name}.stop();'
+            else:
+                lizard_code += f'{z_axis.name}_z.speed(0);'
+                lizard_code += f'{z_axis.name}_y.speed(0);'
         if isinstance(flashlight, FlashlightHardware):
             lizard_code += f' {flashlight.name}.on();'
         elif isinstance(flashlight, FlashlightHardwareV2):
@@ -59,6 +65,9 @@ class SafetyHardware(Safety, rosys.hardware.ModuleHardware):
             lizard_code += f'when {y_axis.name}_ref_t.level == 1 then {wheels.name}.speed(0, 0); end\n'
         if isinstance(z_axis, ZAxisHardware) or isinstance(z_axis, ZAxisHardwareV2):
             lizard_code += f'when {z_axis.name}_ref_t.level == {1 if not z_axis.ccw else -1} then {wheels.name}.speed(0, 0); {y_axis.name}.stop(); end\n'
+        if isinstance(z_axis, TornadoHardware):
+            lizard_code += f'when {z_axis.name}_ref_t.level == 1 then {wheels.name}.speed(0, 0); {y_axis.name}.stop(); end\n'
+            lizard_code += f'when {z_axis.name}_end_bottom.level == 0 then {wheels.name}.speed(0, 0); {y_axis.name}.stop(); end\n'
 
         lizard_code += f'when core.last_message_age > 1000 then {wheels.name}.speed(0, 0); end\n'
         lizard_code += 'when core.last_message_age > 20000 then stop(); end\n'
