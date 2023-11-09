9a77e2c627d02b175025c0ac1183ff81db0cd1f8
Miguel <angelom93@web.de>
2023-11-09 08:13:16 +0100

    added flashlight pwm module

--- field_friend/hardware/flashlight_pwm.py
+++ field_friend/hardware/flashlight_pwm.py
@@ -27,36 +27,56 @@ class FlashlightPWM(rosys.hardware.Module, abc.ABC):
 
 class FlashlightPWMHardware(FlashlightPWM, rosys.hardware.ModuleHardware):
 
-    def __init__(self, robot_brain: rosys.hardware.RobotBrain, *,
+    UPDATE_INTERVAL = 5.0
+
+    def __init__(self, robot_brain: rosys.hardware.RobotBrain,
+                 bms: rosys.hardware.Bms, *,
                  expander: Optional[rosys.hardware.ExpanderHardware],
                  name: str = 'flashlight',
-                 front_pin: int = 12,
-                 back_pin: int = 23,
-                 duty_cycle: int = 0,
-                 frequency: int = 0) -> None:
+                 pin: int = 12,
+                 rated_voltage: float = 23.0,
+                 ) -> None:
         self.name = name
         self.expander = expander
-        self.duty_cycle = duty_cycle
-        self.frequency = frequency
+        self.bms = bms
+        self.rated_voltage = rated_voltage
+        self.last_update: float = 0
+        self.duty_cycle: float = 0.1
         lizard_code = remove_indentation(f'''
-            {name}_front = {expander.name + "." if expander else ""}PwmOutput({front_pin})
-            {name}_back = {expander.name + "." if expander else ""}PwmOutput({back_pin})
+            {name} = {expander.name + "." if expander else ""}PwmOutput({pin})
+            {name}.duty = 20
         ''')
         super().__init__(robot_brain=robot_brain, lizard_code=lizard_code)
+        rosys.on_repeat(self._set_duty_cycle, 1.0)
 
     async def turn_on(self) -> None:
         await super().turn_on()
         await self.robot_brain.send(
-            f'{self.name}_front.on();'
-            f'{self.name}_back.on();'
+            f'{self.name}.on();'
         )
 
     async def turn_off(self) -> None:
         await super().turn_off()
         await self.robot_brain.send(
-            f'{self.name}_front.off();'
-            f'{self.name}_back.off();'
+            f'{self.name}.off();'
         )
+
+    async def _set_duty_cycle(self) -> None:
+        if rosys.time() > self.last_update + self.UPDATE_INTERVAL:
+
+            self.last_update = rosys.time()
+            current_voltage = self.bms.state.voltage
+            if current_voltage is None:
+                return
+            self.duty_cycle = self.rated_voltage / current_voltage
+            if self.duty_cycle > 1:
+                self.duty_cycle = 1
+            # get a 8 bit value for the duty cycle (0-255) no negative values
+            duty = int(self.duty_cycle * 255)
+
+            await self.robot_brain.send(
+                f'{self.name}.duty={duty};'
+            )
 
 
 class FlashlightPWMSimulation(FlashlightPWM, rosys.hardware.ModuleSimulation):
