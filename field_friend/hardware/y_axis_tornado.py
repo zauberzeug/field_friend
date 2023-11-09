7fe5e25890af2fad49d6f01c32e199c42b958d5f
Miguel <angelom93@web.de>
2023-11-09 23:50:25 +0100

    made y_axis_tornado configurable

--- field_friend/hardware/y_axis_tornado.py
+++ field_friend/hardware/y_axis_tornado.py
@@ -8,14 +8,14 @@ from rosys.helpers import remove_indentation
 class YAxisTornado(rosys.hardware.Module, abc.ABC):
     """The y axis module is a simple example for a representation of real or simulated robot hardware."""
 
-    MAX_SPEED: int = 80_000
-    MIN_POSITION: float = -0.12
-    MAX_POSITION: float = 0.12
-    AXIS_OFFSET = 0.123
-    STEPS_PER_M: float = 666.67 * 1000
-
-    def __init__(self, **kwargs) -> None:
+    def __init__(self, max_speed, min_position, max_position, axis_offset, steps_per_m, **kwargs) -> None:
         super().__init__(**kwargs)
+
+        self.max_speed: int = max_speed
+        self.min_position: float = min_position
+        self.max_position: float = max_position
+        self.axis_offset: float = axis_offset
+        self.steps_per_m: float = steps_per_m
 
         self.steps: int = 0
         self.alarm: bool = False
@@ -36,11 +36,11 @@ class YAxisTornado(rosys.hardware.Module, abc.ABC):
     async def move_to(self, position: float, speed: int) -> None:
         if not self.is_referenced:
             raise RuntimeError('yaxis is not referenced, reference first')
-        if speed > self.MAX_SPEED:
-            raise RuntimeError(f'yaxis speed is too high, max speed is {self.MAX_SPEED}')
-        if not self.MIN_POSITION <= position <= self.MAX_POSITION:
+        if speed > self.max_speed:
+            raise RuntimeError(f'yaxis speed is too high, max speed is {self.max_speed}')
+        if not self.min_position <= position <= self.max_position:
             raise RuntimeError(
-                f'target position {position} ist out of yaxis range {self.MIN_POSITION} to {self.MAX_POSITION}')
+                f'target position {position} ist out of yaxis range {self.min_position} to {self.max_position}')
 
     @abc.abstractmethod
     async def try_reference(self) -> bool:
@@ -51,10 +51,10 @@ class YAxisTornado(rosys.hardware.Module, abc.ABC):
 
         The position is given in meters.
         """
-        return int((position + self.AXIS_OFFSET) * self.STEPS_PER_M)
+        return int((position + self.axis_offset) * self.steps_per_m)
 
     def compute_position(self, steps: int) -> float:
-        return steps / self.STEPS_PER_M + self.AXIS_OFFSET
+        return steps / self.steps_per_m + self.axis_offset
 
     @property
     def position(self) -> float:
@@ -67,6 +67,11 @@ class YAxisHardwareTornado(YAxisTornado, rosys.hardware.ModuleHardware):
     def __init__(self, robot_brain: rosys.hardware.RobotBrain, *,
                  name: str = 'yaxis_tornado',
                  expander: Optional[rosys.hardware.ExpanderHardware],
+                 max_speed: int = 80_000,
+                 min_position: float = -0.12,
+                 max_position: float = 0.12,
+                 axis_offset: float = 0.123,
+                 steps_per_m: float = 666.67 * 1000,
                  step_pin: int = 19,
                  dir_pin: int = 18,
                  alarm_pin: int = 35,
@@ -78,10 +83,10 @@ class YAxisHardwareTornado(YAxisTornado, rosys.hardware.ModuleHardware):
         self.name = name
         self.expander = expander
         lizard_code = remove_indentation(f'''
-            {name}_motor = {expander.name + "." if motor_on_expander == True else ""}StepperMotor({step_pin}, {dir_pin})
-            {name}_alarm = {expander.name + "." if motor_on_expander == True else ""}Input({alarm_pin})
-            {name}_end_l = {expander.name + "." if end_stops_on_expander == True else ""}Input({end_l_pin})
-            {name}_end_r = {expander.name + "." if end_stops_on_expander == True else ""}Input({end_r_pin})
+            {name}_motor = {expander.name + "." if motor_on_expander and expander else ""}StepperMotor({step_pin}, {dir_pin})
+            {name}_alarm = {expander.name + "." if motor_on_expander and expander else ""}Input({alarm_pin})
+            {name}_end_l = {expander.name + "." if end_stops_on_expander and expander else ""}Input({end_l_pin})
+            {name}_end_r = {expander.name + "." if end_stops_on_expander and expander else ""}Input({end_r_pin})
             {name} = MotorAxis({name}_motor, {name}_end_l, {name}_end_r)
         ''')
         core_message_fields = [
@@ -91,13 +96,23 @@ class YAxisHardwareTornado(YAxisTornado, rosys.hardware.ModuleHardware):
             f'{name}_motor.position',
             f'{name}_alarm.level'
         ]
-        super().__init__(robot_brain=robot_brain, lizard_code=lizard_code, core_message_fields=core_message_fields)
+        super().__init__(
+            max_speed=max_speed,
+            min_position=min_position,
+            max_position=max_position,
+            axis_offset=axis_offset,
+            steps_per_m=steps_per_m,
+            robot_brain=robot_brain,
+            lizard_code=lizard_code,
+            core_message_fields=core_message_fields)
 
     async def stop(self) -> None:
         await super().stop()
         await self.robot_brain.send(f'{self.name}.stop()')
 
-    async def move_to(self, position: float, speed: int = YAxisTornado.MAX_SPEED) -> None:
+    async def move_to(self, position: float, speed: int = 0) -> None:
+        if speed == 0:
+            speed = self.max_speed
         try:
             await super().move_to(position, speed)
         except RuntimeError as error:
@@ -124,35 +139,35 @@ class YAxisHardwareTornado(YAxisTornado, rosys.hardware.ModuleHardware):
 
             # if in end l stop, disable end stops and move out
             if self.end_l:
-                await self.robot_brain.send(f'{self.name}.speed(-{self.MAX_SPEED/10});')
+                await self.robot_brain.send(f'{self.name}.speed(-{self.max_speed/10});')
                 while self.end_r:
                     await rosys.sleep(0.2)
                 await self.robot_brain.send(f'{self.name}.stop();')
 
             # if in end r stop, move out
             if self.end_r:
-                await self.robot_brain.send(f'{self.name}.speed({self.MAX_SPEED/10});')
+                await self.robot_brain.send(f'{self.name}.speed({self.max_speed/10});')
                 while self.end_r:
                     await rosys.sleep(0.2)
                 await self.robot_brain.send(f'{self.name}.stop();')
 
             # move to end r stop
-            await self.robot_brain.send(f'{self.name}.speed(-{self.MAX_SPEED/6});')
+            await self.robot_brain.send(f'{self.name}.speed(-{self.max_speed/6});')
             while not self.end_r:
                 await rosys.sleep(0.2)
 
             # move out of end r stop
-            await self.robot_brain.send(f'{self.name}.speed({self.MAX_SPEED/6});')
+            await self.robot_brain.send(f'{self.name}.speed({self.max_speed/6});')
             while self.end_r:
                 await rosys.sleep(0.2)
 
             # move slowly to end r stop
-            await self.robot_brain.send(f'{self.name}.speed(-{self.MAX_SPEED/10});')
+            await self.robot_brain.send(f'{self.name}.speed(-{self.max_speed/10});')
             while not self.end_r:
                 await rosys.sleep(0.2)
 
             # move slowly out of end r stop
-            await self.robot_brain.send(f'{self.name}.speed({self.MAX_SPEED/10});')
+            await self.robot_brain.send(f'{self.name}.speed({self.max_speed/10});')
             while self.end_r:
                 await rosys.sleep(0.2)
 
@@ -184,18 +199,31 @@ class YAxisSimulationTornado(YAxisTornado, rosys.hardware.ModuleSimulation):
     '''The y axis simulation module is a simple example for a representation of simulated robot hardware.
     '''
 
-    def __init__(self) -> None:
-        super().__init__()
-
+    def __init__(self,
+                 max_speed: int = 80_000,
+                 min_position: float = -0.12,
+                 max_position: float = 0.12,
+                 axis_offset: float = 0.123,
+                 steps_per_m: float = 666.67 * 1000,
+                 ) -> None:
         self.speed: int = 0
         self.target_steps: Optional[int] = None
+        super().__init__(
+            max_speed=max_speed,
+            min_position=min_position,
+            max_position=max_position,
+            axis_offset=axis_offset,
+            steps_per_m=steps_per_m,
+        )
 
     async def stop(self) -> None:
         await super().stop()
         self.speed = 0
         self.target_steps = None
 
-    async def move_to(self, position: float, speed: int = 80_000) -> None:
+    async def move_to(self, position: float, speed: int = 0) -> None:
+        if speed == 0:
+            speed = self.max_speed
         try:
             await super().move_to(position, speed)
         except RuntimeError as e:
@@ -216,6 +244,7 @@ class YAxisSimulationTornado(YAxisTornado, rosys.hardware.ModuleSimulation):
     async def step(self, dt: float) -> None:
         await super().step(dt)
         self.steps += int(dt * self.speed)
+        self.idle = self.speed == 0
         if self.target_steps is not None:
             if (self.speed > 0) == (self.steps > self.target_steps):
                 self.steps = self.target_steps
