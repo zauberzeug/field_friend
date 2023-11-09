fd9eebf8b2059cd0b3350e5e6cad49939531f766
Miguel <angelom93@web.de>
2023-11-09 08:13:31 +0100

    updatetd safety module

--- field_friend/hardware/safety.py
+++ field_friend/hardware/safety.py
@@ -4,8 +4,8 @@ from typing import Union
 import rosys
 
 from .chain_axis import ChainAxis, ChainAxisHardware, ChainAxisSimulation
-from .double_wheels import DoubleWheelsHardware
 from .flashlight import Flashlight, FlashlightHardware, FlashlightSimulation
+from .flashlight_pwm import FlashlightPWM, FlashlightPWMHardware, FlashlightPWMSimulation
 from .flashlight_v2 import FlashlightHardwareV2, FlashlightSimulationV2, FlashlightV2
 from .y_axis import YAxis, YAxisHardware, YAxisSimulation
 from .z_axis import ZAxis, ZAxisHardware, ZAxisSimulation
@@ -20,7 +20,7 @@ class Safety(rosys.hardware.Module, abc.ABC):
                  estop: rosys.hardware.EStop,
                  y_axis: Union[YAxis, ChainAxis, None] = None,
                  z_axis: Union[ZAxis, ZAxisV2, None] = None,
-                 flashlight: Union[Flashlight, FlashlightV2] = None,
+                 flashlight: Union[Flashlight, FlashlightV2, FlashlightPWM, None] = None,
                  **kwargs) -> None:
         super().__init__(**kwargs)
         self.wheels = wheels
@@ -38,7 +38,8 @@ class SafetyHardware(Safety, rosys.hardware.ModuleHardware):
                  estop: rosys.hardware.EStop,
                  y_axis: Union[YAxisHardware, ChainAxisHardware, None] = None,
                  z_axis: Union[ZAxisHardware, ZAxisHardwareV2, None] = None,
-                 flashlight: Union[FlashlightHardware, FlashlightHardwareV2, None]) -> None:
+                 flashlight: Union[FlashlightHardware, FlashlightHardwareV2, FlashlightPWMHardware, None],
+                 ) -> None:
 
         lizard_code = f'let stop do {wheels.name}.speed(0, 0);'
         if y_axis is not None:
@@ -49,6 +50,8 @@ class SafetyHardware(Safety, rosys.hardware.ModuleHardware):
             lizard_code += f' {flashlight.name}.on();'
         elif isinstance(flashlight, FlashlightHardwareV2):
             lizard_code += f' {flashlight.name}_front.off(); {flashlight.name}_back.off();'
+        elif isinstance(flashlight, FlashlightPWMHardware):
+            lizard_code += f' {flashlight.name}.off();'
         lizard_code += 'end\n'
         for name in estop.pins:
             lizard_code += f'when estop_{name}.level == 0 then stop(); end\n'
@@ -58,7 +61,7 @@ class SafetyHardware(Safety, rosys.hardware.ModuleHardware):
             lizard_code += f'when {z_axis.name}_ref_t.level == {1 if not z_axis.ccw else -1} then {wheels.name}.speed(0, 0); {y_axis.name}.stop(); end\n'
 
         lizard_code += f'when core.last_message_age > 1000 then {wheels.name}.speed(0, 0); end\n'
-        lizard_code += f'when core.last_message_age > 20000 then stop(); end\n'
+        lizard_code += 'when core.last_message_age > 20000 then stop(); end\n'
 
         super().__init__(wheels=wheels,
                          estop=estop,
@@ -77,15 +80,15 @@ class SafetySimulation(Safety, rosys.hardware.ModuleSimulation):
                  estop: rosys.hardware.EStop,
                  y_axis: Union[YAxisSimulation, ChainAxisSimulation, None] = None,
                  z_axis: Union[ZAxisSimulation, ZAxisSimulationV2, None] = None,
-                 flashlight: Union[FlashlightSimulation, FlashlightSimulationV2, None]) -> None:
+                 flashlight: Union[FlashlightSimulation, FlashlightSimulationV2, FlashlightPWMSimulation, None]) -> None:
         super().__init__(wheels=wheels, estop=estop, y_axis=y_axis, z_axis=z_axis, flashlight=flashlight)
 
     async def step(self, dt: float) -> None:
         if self.estop.active:
             self.wheels.stop()
             if self.y_axis is not None:
-                self.y_axis.stop()
+                await self.y_axis.stop()
             if self.z_axis is not None:
-                self.z_axis.stop()
+                await self.z_axis.stop()
             if self.flashlight is not None:
-                self.flashlight.turn_off()
+                await self.flashlight.turn_off()
