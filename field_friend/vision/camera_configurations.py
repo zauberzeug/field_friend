131d3d8cff492c17faf77892ae564bdd1be0184b
Miguel <angelom93@web.de>
2024-01-15 14:51:01 +0100

    add camera configurations

---
+++ field_friend/vision/camera_configurations.py
@@ -0,0 +1,57 @@
+configurations = {
+    'u1': {
+        'parameters': {
+            'width': 1920,
+            'height': 1080,
+            'auto_exposure': True,
+        },
+        'crop': {
+            'xoffset': 420,
+            'yoffset': 30,
+        },
+    },
+    'u2': {
+        'parameters': {
+            'width': 1920,
+            'height': 1080,
+            'auto_exposure': True,
+        },
+        'crop': {
+            'xoffset': 420,
+            'yoffset': 150,
+        },
+    },
+    'u3': {
+        'parameters': {
+            'width': 1920,
+            'height': 1080,
+            'auto_exposure': True,
+        },
+        'crop': {
+            'xoffset': 400,
+            'yoffset': 100,
+        },
+    },
+    'u4': {
+        'parameters': {
+            'width': 1920,
+            'height': 1080,
+            'auto_exposure': True,
+        },
+        'crop': {
+            'xoffset': 400,
+            'yoffset': 100,
+        },
+    },
+    'ff3': {
+        'parameters': {
+            'width': 1920,
+            'height': 1080,
+            'auto_exposure': True,
+        },
+        'crop': {
+            'xoffset': 420,
+            'yoffset': 150,
+        },
+    },
+}
