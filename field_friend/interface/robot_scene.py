759a4f123a39891e8f0f7f35b87df5bbe926a241
Miguel <angelom93@web.de>
2024-02-29 16:28:12 +0100

    remove grid from ui scene

--- field_friend/interface/robot_scene.py
+++ field_friend/interface/robot_scene.py
@@ -23,7 +23,7 @@ class robot_scene:
         self.scene_look = False
 
         with self.scene_card.tight().classes('w-full place-items-center').style('max-width: 100%; overflow: hidden;'):
-            with ui.scene(200, 200, on_click=self.handle_click).classes('w-full') as self.scene:
+            with ui.scene(200, 200, on_click=self.handle_click, grid=False).classes('w-full') as self.scene:
                 field_friend_object(self.system.odometer, self.system.usb_camera_provider, self.system.field_friend)
                 rosys.driving.driver_object(self.system.driver)
                 plant_objects(self.system.plant_provider, self.system.big_weed_category_names +
@@ -41,11 +41,10 @@ class robot_scene:
 
     def update(self) -> None:
         position = self.system.odometer.prediction.point
+        relative_camera_position = self.system.odometer.prediction.transform(rosys.geometry.Point(x=0.1, y=0.5))
         if self.scene_look:
             height = 10.0
         else:
-            height = 1.1
-        y = position.polar(0.8, self.system.odometer.prediction.yaw_deg-90).y
-        x = position.polar(0.1, self.system.odometer.prediction.yaw_deg+20).x
-        self.scene.move_camera(x=x, y=y, z=height,
+            height = 1.2
+        self.scene.move_camera(x=relative_camera_position.x, y=relative_camera_position.y, z=height,
                                look_at_x=position.x, look_at_y=position.y, duration=0.0)
