ddaf1e465176a2645f110aaea471076bfd701e9d
Miguel <angelom93@web.de>
2024-03-01 10:20:43 +0100

    Refactor AutomationWatcher constructor and methods

--- field_friend/automations/automation_watcher.py
+++ field_friend/automations/automation_watcher.py
@@ -1,16 +1,13 @@
 import logging
 from copy import deepcopy
-from typing import Optional
+from typing import TYPE_CHECKING, Optional
 
 import rosys
-from rosys.automation import Automator
-from rosys.driving import Odometer
 from rosys.geometry import Pose
 from shapely.geometry import Polygon
 
-from ..hardware import FieldFriend
-from ..navigation import Gnss
-from . import FieldProvider
+if TYPE_CHECKING:
+    from system import System
 
 DEFAULT_RESUME_DELAY = 1.0
 RESET_POSE_DISTANCE = 1.0
@@ -18,13 +15,13 @@ RESET_POSE_DISTANCE = 1.0
 
 class AutomationWatcher:
 
-    def __init__(self, automator: Automator, odometer: Odometer, field_friend: FieldFriend, gnss: Gnss, field_provider: FieldProvider) -> None:
+    def __init__(self, system: 'System') -> None:
         self.log = logging.getLogger('field_friend.automation_watcher')
 
-        self.automator = automator
-        self.odometer = odometer
-        self.field_friend = field_friend
-        self.gnss = gnss
+        self.automator = system.automator
+        self.odometer = system.odometer
+        self.field_friend = system.field_friend
+        self.gnss = system.gnss
 
         self.try_resume_active: bool = False
         self.incidence_time: float = 0.0
@@ -34,6 +31,7 @@ class AutomationWatcher:
         self.field_polygong: Optional[Polygon] = None
 
         rosys.on_repeat(self.try_resume, 0.1)
+        rosys.on_repeat(self.check_field_bounds, 1.0)
         if self.field_friend.bumper:
             self.field_friend.bumper.BUMPER_TRIGGERED.register(
                 lambda name: self.pause(f'the {name} bumper was triggered'))
@@ -51,22 +49,23 @@ class AutomationWatcher:
             self.try_resume_active = False
 
     def try_resume(self) -> None:
-        self.automator.enabled = not bool(self.bumper.active_bumpers)
+        if self.field_friend.bumper:
+            self.automator.enabled = not bool(self.field_friend.bumper.active_bumpers)
 
-        if self.try_resume_active and self.automator.is_running:
-            self.log.info('disabling auto-resume because automation is already running again')
-            self.try_resume_active = False
+            if self.try_resume_active and self.automator.is_running:
+                self.log.info('disabling auto-resume because automation is already running again')
+                self.try_resume_active = False
 
-        if self.try_resume_active and rosys.time() > self.incidence_time + self.resume_delay and not self.bumper.active_bumpers:
-            self.log.info(f'resuming automation after {self.resume_delay:.0f}s')
-            self.automator.resume()
-            self.resume_delay += 2
-            self.try_resume_active = False
+            if self.try_resume_active and rosys.time() > self.incidence_time + self.resume_delay and not self.field_friend.bumper.active_bumpers:
+                self.log.info(f'resuming automation after {self.resume_delay:.0f}s')
+                self.automator.resume()
+                self.resume_delay += 2
+                self.try_resume_active = False
 
-        if self.odometer.prediction.distance(self.incidence_pose) > RESET_POSE_DISTANCE:
-            if self.resume_delay != DEFAULT_RESUME_DELAY:
-                self.log.info('resetting resume_delay')
-                self.resume_delay = DEFAULT_RESUME_DELAY
+            if self.odometer.prediction.distance(self.incidence_pose) > RESET_POSE_DISTANCE:
+                if self.resume_delay != DEFAULT_RESUME_DELAY:
+                    self.log.info('resetting resume_delay')
+                    self.resume_delay = DEFAULT_RESUME_DELAY
 
     def check_field_bounds(self) -> None:
         if not self.field_boundaries:
