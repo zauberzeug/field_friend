1a2b3f02e2d4cb98f22b156273959018d54a8b16
Lukas Baecker <baecker.lukas@outlook.de>
2024-03-08 11:48:14 +0100

    robot info dialog

---
+++ field_friend/interface/info.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+from nicegui import background_tasks, ui
+from nicegui.version import __version__ as nicegui_version
+from rosys.version import __version__ as rosys_version
+
+if TYPE_CHECKING:
+    from ..system import System
+
+MUTEX_PATH = Path('.livesync_mutex')
+
+
+class Info:
+
+    def __init__(self, system: System) -> None:
+        self.system = system
+
+    def create_dialog(self) -> ui.dialog:
+        with ui.dialog() as dialog, ui.card():
+            ui.label('Versions').classes('text-xl font-semibold')
+            with ui.grid(columns=2):
+                ui.label('Fieldfriend:')
+                ui.label(self.system.version)
+                ui.label('RoSys:')
+                ui.label(rosys_version)
+                ui.label('NiceGUI:')
+                ui.label(nicegui_version)
+
+                if self.system.is_real:
+                    lizard_firmware = self.system.field_friend.robot_brain.lizard_firmware
+
+                    async def read_lizard_versions() -> None:
+                        await lizard_firmware.read_core_version()
+                        await lizard_firmware.read_p0_version()
+                    background_tasks.create(read_lizard_versions())
+
+                    ui.label('Lizard (Core):')
+                    ui.label().bind_text_from(lizard_firmware, 'core_version')
+                    ui.label('Lizard (P0):')
+                    ui.label().bind_text_from(lizard_firmware, 'p0_version')
+
+            if MUTEX_PATH.exists():
+                ui.label('Livesync').classes('text-xl font-semibold')
+                ui.code(MUTEX_PATH.read_text().strip(), language=None)
+
+        return dialog
--- field_friend/system.py
+++ field_friend/system.py
@@ -3,6 +3,7 @@ import os
 
 import numpy as np
 import rosys
+from rosys.analysis import KpiLogger
 
 from field_friend.automations import (AutomationWatcher, BatteryWatcher, CoinCollecting, FieldProvider, Mowing,
                                       PathProvider, PathRecorder, PlantLocator, PlantProvider, Puncher, Weeding)
@@ -10,20 +11,23 @@ from field_friend.hardware import FieldFriendHardware, FieldFriendSimulation
 from field_friend.navigation import GnssHardware, GnssSimulation
 from field_friend.vision import CameraConfigurator, SimulatedCam, SimulatedCamProvider, UsbCamProvider
 
+from .interface.info import Info
+from .kpi_generator import generate_kpis
+
 
 class System:
     def __init__(self) -> None:
         rosys.hardware.SerialCommunication.search_paths.insert(0, '/dev/ttyTHS0')
         self.log = logging.getLogger('field_friend.system')
         self.is_real = rosys.hardware.SerialCommunication.is_possible()
-        version = 'u4'  # insert here your field friend version
+        self.version = 'u4'  # insert here your field friend version
         if self.is_real:
-            self.field_friend = FieldFriendHardware(version=version)
+            self.field_friend = FieldFriendHardware(version=self.version)
             self.usb_camera_provider = UsbCamProvider()
             self.detector = rosys.vision.DetectorHardware(port=8004)
             # self.circle_sight = CircleSight()
         else:
-            self.field_friend = FieldFriendSimulation(version=version)
+            self.field_friend = FieldFriendSimulation(version=self.version)
             self.usb_camera_provider = SimulatedCamProvider()
             self.usb_camera_provider.remove_all_cameras()
             self.usb_camera_provider.add_camera(SimulatedCam.create_calibrated(id='bottom_cam',
@@ -33,7 +37,7 @@ class System:
                                                                                yaw=np.deg2rad(90)))
             self.detector = rosys.vision.DetectorSimulation(self.usb_camera_provider)
             # self.circle_sight = None
-        self.camera_configurator = CameraConfigurator(self.usb_camera_provider, version)
+        self.camera_configurator = CameraConfigurator(self.usb_camera_provider, self.version)
         self.plant_provider = PlantProvider()
         self.field_provider = FieldProvider()
         self.steerer = rosys.driving.Steerer(self.field_friend.wheels, speed_scaling=0.25)
@@ -52,19 +56,32 @@ class System:
         self.driver.parameters.carrot_distance = 0.2
         self.driver.parameters.carrot_offset = self.driver.parameters.hook_offset + self.driver.parameters.carrot_distance
         self.puncher = Puncher(self.field_friend, self.driver)
+
+        self.kpi_logger = KpiLogger()
+        if not (self.is_real):
+            generate_kpis(self.kpi_logger)
+
+        def watch_robot() -> None:
+            self.kpi_logger.increment_on_rising_edge('bumps', bool(self.field_friend.bumper.active_bumpers))
+            self.kpi_logger.increment_on_rising_edge('low_battery', self.field_friend.bms.is_below_percent(10.0))
+            # TODO add more things to watch after
+
         self.big_weed_category_names = ['thistle', 'big_weed', 'orache']
         self.small_weed_category_names = ['weed', 'coin']
         self.crop_category_names = ['sugar_beet', 'crop', 'coin_with_hole']
-        self.plant_locator = PlantLocator(self.usb_camera_provider, self.detector, self.plant_provider, self.odometer)
+        self.plant_locator = PlantLocator(self.usb_camera_provider, self.detector,
+                                          self.plant_provider, self.odometer, self.kpi_logger)
         self.plant_locator.weed_category_names = self.big_weed_category_names + self.small_weed_category_names
         self.plant_locator.crop_category_names = self.crop_category_names
+
+        rosys.on_repeat(watch_robot, 1.0)
+        # TODO außerdem loggen wenn etwas in den Automationen passiert (überlegen wo dann genau geloggt wird)
 
         self.weeding = Weeding(self)
         self.coin_collecting = CoinCollecting(self)
         self.path_provider = PathProvider()
         self.path_recorder = PathRecorder(self.path_provider, self.driver, self.steerer, self.gnss)
         self.field_provider = FieldProvider()
-
         width = 0.64
         length = 0.78
         offset = 0.36
@@ -79,7 +96,7 @@ class System:
             height=height)
         self.path_planner = rosys.pathplanning.PathPlanner(self.shape)
         self.mowing = Mowing(self.field_friend, self.field_provider, driver=self.driver,
-                             path_planner=self.path_planner, gnss=self.gnss, robot_width=width)
+                             path_planner=self.path_planner, gnss=self.gnss, robot_width=width, kpi_logger=self.kpi_logger)
 
         self.automations = {
             'weeding': self.weeding.start,
@@ -88,6 +105,7 @@ class System:
         }
         self.automator = rosys.automation.Automator(None, on_interrupt=self.field_friend.stop,
                                                     default_automation=self.coin_collecting.start)
+        self.info = Info(self)
         if self.field_friend.bumper is not None:
             self.automation_watcher = AutomationWatcher(self.automator, self.odometer, self.field_friend.bumper)
         if self.is_real:
