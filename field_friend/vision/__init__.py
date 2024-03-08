f2609709bd7992e0440e8a729d984e791b7c832e
Miguel <angelom93@web.de>
2024-03-08 16:29:32 +0100

    Remove CircleSight class and its dependencies

--- field_friend/vision/__init__.py
+++ field_friend/vision/__init__.py
@@ -1,14 +1,12 @@
 from .calibration import DOT_DISTANCE, Contour, Dot, Network
 from .camera_configurations import configurations
 from .camera_configurator import CameraConfigurator
-from .circle_sight import CircleSight
 from .simulated_cam import SimulatedCam
 from .simulated_cam_provider import SimulatedCamProvider
 from .usb_cam import UsbCam
 from .usb_cam_provider import UsbCamProvider
 
 __all__ = [
-    'CircleSight',
     'UsbCam',
     'UsbCamProvider',
     'SimulatedCam',
