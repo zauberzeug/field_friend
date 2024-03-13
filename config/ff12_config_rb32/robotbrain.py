8e5b44dfbc9e76d87682086ee1290d89943c5f1b
Johannes-Thiel <jojo.thadfunn@gmail.com>
2024-03-13 15:03:40 +0100

    Update hardware and robotbrain configurations for ff12

--- config/ff12_config_rb32/robotbrain.py
+++ config/ff12_config_rb32/robotbrain.py
@@ -1,25 +1,3 @@
 configuration = {'robot_brain': {
     'flash_params': ['orin', 'v05']
-},
-    'bluetooth': {
-        'name': 'fieldfriend-ff12',
-},
-    'serial': {
-        'name': 'serial',
-        'rx_pin': 26,
-        'tx_pin': 27,
-        'baud': 115200,
-        'num': 1,
-},
-    'expander': {
-        'name': 'p0',
-        'boot': 25,
-        'enable': 14,
-},
-    'can': {
-        'name': 'can',
-        'on_expander': False,
-        'rx_pin': 32,
-        'tx_pin': 33,
-        'baud': 1_000_000,
 }}
