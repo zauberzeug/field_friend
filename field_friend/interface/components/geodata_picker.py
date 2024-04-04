eafb9ab8b8294f1a4ac0bee174a6db8186ca354c
Lukas Baecker <baecker.lukas@outlook.de>
2024-04-04 09:59:01 +0200

    create rows in a fixed row spacing, check if row is inside of field

--- field_friend/interface/components/geodata_picker.py
+++ field_friend/interface/components/geodata_picker.py
@@ -1,6 +1,4 @@
 import json
-import platform
-import tempfile
 import uuid
 import xml.etree.ElementTree as ET
 from pathlib import Path
@@ -10,9 +8,11 @@ import fiona
 import geopandas as gpd
 import rosys
 from nicegui import events, ui
+from shapely import offset_curve
+from shapely.geometry import LineString, Polygon, mapping
 from shapely.ops import transform
 
-from ...automations import Field, FieldProvider
+from ..automations import Field, FieldProvider, Row
 
 # Enable fiona driver
 fiona.drvsupport.supported_drivers['kml'] = 'rw'  # enable KML support which is disabled by default
@@ -24,6 +24,12 @@ class geodata_picker(ui.dialog):
     def __init__(self, field_provider: FieldProvider) -> None:
         super().__init__()
         self.field_provider = field_provider
+        self.is_farmdroid = False
+        self.safety_distance = 2.7
+        self.working_width = 2.7
+        self.headland_tracks = 1
+        self.ab_line = 1
+
         with self, ui.card():
             with ui.row():
                 ui.label("Upload a file.").classes('text-xl w-80')
@@ -35,8 +41,40 @@ class geodata_picker(ui.dialog):
                     "If you want to upload a shape,  create a zip-file containing all files  (minimum: .shp, .shx, .dbf) and upload the zip.").classes('w-80')
             with ui.row():
                 ui.upload(on_upload=self.restore_from_file, multiple=False)
+            with ui.row():
+                farmdroid_checkbox = ui.checkbox('FarmDroid Data', on_change=lambda e: self.set_farmdroid(e.value))
+            with ui.row().bind_visibility_from(farmdroid_checkbox, 'value').classes('w-80'):
+                with ui.row().classes('w-full place-items-center'):
+                    with ui.icon('help').classes('text-xl'):
+                        ui.tooltip('The width of the attached tool.')
+                    ui.number(label='Working Width (in m)', value=self.working_width, format='%.2f',
+                              on_change=lambda e: self.set_working_width(e.value))
+                with ui.row().classes('w-full place-items-center'):
+                    with ui.icon('help').classes('text-xl'):
+                        ui.tooltip('The distance between the border of the field and the headland.')
+                    ui.number(label='Safety Distance (in m)', value=self.safety_distance, format='%.2f',
+                              on_change=lambda e: self.set_safety_distance(e.value))
+                with ui.row().classes('w-full place-items-center'):
+                    with ui.icon('help').classes('text-xl'):
+                        ui.tooltip('Number of headland tracks.')
+                    ui.number(label='headland Tracks', value=self.headland_tracks, format='%.2f',
+                              on_change=lambda e: self.set_headland_tracks(e.value))
+                    # TODO EINGABE FÜR DAS OFFSET DER FARMDROID DATEN
+
             with ui.row().classes('w-full justify-end'):
                 ui.button('Cancel', on_click=self.close).props('outline')
+
+    def set_farmdroid(self, value) -> None:
+        self.is_farmdroid = value
+
+    def set_safety_distance(self, value) -> None:
+        self.safety_distance = value
+
+    def set_working_width(self, value) -> None:
+        self.working_width = value
+
+    def set_headland_tracks(self, value) -> None:
+        self.headland_tracks = value
 
     def extract_coordinates_kml(self, event: events.UploadEventArguments) -> list:
         coordinates = []
@@ -93,11 +131,80 @@ class geodata_picker(ui.dialog):
         if coordinates is None:
             rosys.notify("An error occurred while importing the file.", type='negative')
             return
-        if len(coordinates) > 1 and coordinates[0] == coordinates[-1]:
-            coordinates.pop()
+        lines = []
+        if self.is_farmdroid:
+            if self.safety_distance + (self.working_width / 2) < 2.7:
+                rosys.notify(
+                    'The distance between the outer headland track and the field boundary is too small. Please check the specified values.')
+                return
+            buffer_width = self.safety_distance + (self.headland_tracks * self.working_width)
+            field_border_polygon = Polygon(coordinates)
+            field_border_gdf = gpd.GeoDataFrame(index=[0], crs='epsg:4326', geometry=[field_border_polygon])
+            field_border_gdf_meter = field_border_gdf.to_crs(epsg=3395)
+            buffered_field_meter = field_border_gdf_meter.buffer(distance=-buffer_width, cap_style=3, resolution=26)
+            buffered_field = buffered_field_meter.to_crs(epsg=4326)
+            buffered_coordinates = mapping(buffered_field)['features'][0]['geometry']['coordinates'][0]
+            buffered_coordinates_meter = mapping(buffered_field_meter)['features'][0]['geometry']['coordinates'][0]
+
+            # TODO die gebufferte Feldgrenze wird nocht nicht ganz richtig berechnet
+
+            # teste ob das buffern nach innen funktioniert indem wir ein neues feld anlegen it dem buffer
+            # test_id = str(uuid.uuid4())
+            # field_test = Field(id=f'{test_id}', name=f'{test_id}', outline_wgs84=buffered_coordinates,
+            #                    reference_lat=buffered_coordinates[0][0], reference_lon=buffered_coordinates[0][1])
+            # self.field_provider.add_field(field_test)
+
+            # AB Linie finden
+            if self.ab_line == 1:
+                ab_line = LineString([(buffered_coordinates[0][0], buffered_coordinates[0][1]),
+                                      (buffered_coordinates[1][0], buffered_coordinates[1][1])])
+                ab_line_meter = LineString([(buffered_coordinates_meter[0][0], buffered_coordinates_meter[0][1]),
+                                            (buffered_coordinates_meter[1][0], buffered_coordinates_meter[1][1])])
+            if self.ab_line == 2:
+                ab_line = LineString([(buffered_coordinates[0][0], buffered_coordinates[0][1]),
+                                      (buffered_coordinates[-1][0], buffered_coordinates[-1][1])])
+            # Über Arbeitsbreite die Reihen berechnen
+            row_spacing = self.working_width / 5
+
+            right_check = offset_curve(ab_line_meter, -row_spacing)
+            print(buffered_field_meter[0])
+            if buffered_field_meter[0].contains(right_check) or buffered_field_meter[0].intersects(right_check):
+                row_spacing = -row_spacing
+            lines_meter = []
+            line_inside = True
+            lines.append(ab_line)
+            lines_meter.append(ab_line_meter)
+            counter = 0
+            while line_inside:
+                line_meter = offset_curve(lines_meter[-1], row_spacing)
+                if buffered_field_meter[0].contains(line_meter) or buffered_field_meter[0].intersects(line_meter):
+                    print('this is true')
+                    lines_meter.append(line_meter)
+                    line_gdf_meter = gpd.GeoDataFrame(index=[0], crs='epsg:3395', geometry=[line_meter])
+                    line_gdf = line_gdf_meter.to_crs(epsg=4326)
+                    lines.append(line_gdf['geometry'][0])
+                    counter += 1
+                else:
+                    print(f'false')
+                    line_inside = False
+            print(counter)
+            # TODO hier müssen an sich die Linien noch so verlängert werden oder gekürzt in beide richtungen dass sie mit dem boundary geschnitten werden
+
+        if len(coordinates) > 2 and coordinates[0] == coordinates[-1]:
+            coordinates.pop()  # the last point is the same as the first point
         reference_point = coordinates[0]
+
+        rows = []
+        for line in lines:
+            point_list = list(mapping(line)['coordinates'])
+            for index, point in enumerate(point_list):
+                point_list[index] = list(point)
+            row_id = str(uuid.uuid4())
+            new_row = Row(id=f'{row_id}', name=f'{row_id}', points_wgs84=point_list)
+            rows.append(new_row)
+
         new_id = str(uuid.uuid4())
         field = Field(id=f'{new_id}', name=f'{new_id}', outline_wgs84=coordinates,
-                      reference_lat=reference_point[0], reference_lon=reference_point[1])
+                      reference_lat=reference_point[0], reference_lon=reference_point[1], rows=rows)
         self.field_provider.add_field(field)
         return
