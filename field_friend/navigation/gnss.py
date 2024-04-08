a6af80e1adc505d61cb6588cda39aa7a211eb611
Miguel <angelom93@web.de>
2024-04-08 09:27:27 +0200

    cleanup gnss

--- field_friend/navigation/gnss.py
+++ field_friend/navigation/gnss.py
@@ -55,7 +55,7 @@ class Gnss(rosys.persistence.PersistentModule, ABC):
         self.reference_lon: Optional[float] = None
 
         self.needs_backup = False
-        rosys.on_repeat(self.update, 0.1)
+        rosys.on_repeat(self.update, 0.05)
         rosys.on_repeat(self.try_connection, 3.0)
 
     def backup(self) -> dict:
@@ -110,11 +110,6 @@ class GnssHardware(Gnss):
         super().__init__()
         self.odometer = odometer
 
-    def __del__(self) -> None:
-        if self.ser and self.ser.isOpen():
-            self.ser.close()
-            self.log.debug('closed gnss serial connection')
-
     async def try_connection(self) -> None:
         await super().try_connection()
         if self.device is not None:
@@ -131,12 +126,13 @@ class GnssHardware(Gnss):
             # self.log.error('No GNSS device found')
             return
 
-        self.log.info(f'Connecting to GNSS device "{self.device}"')
+        self.log.info(f'Connecting to GNSS device "{self.device}"...')
         try:
-            self.ser = serial.Serial(self.device, baudrate=115200)
+            self.ser = serial.Serial(self.device, baudrate=115200, timeout=0.2)
         except serial.SerialException as e:
             self.log.error(f'Could not connect to GNSS device: {e}')
             self.device = None
+        self.log.info(f'Connected to GNSS device "{self.device}"')
 
     async def _read(self) -> Optional[str]:
         if not self.ser.isOpen():
@@ -168,7 +164,7 @@ class GnssHardware(Gnss):
                     msg = await rosys.run.cpu_bound(pynmea2.parse, line)
                     if not hasattr(msg, 'sentence_type'):
                         self.log.info(f'No sentence type: {msg}')
-                        continue
+                        return
                     if msg.sentence_type in self.TYPES_NEEDED:
                         types_seen.add(msg.sentence_type)
                     if msg.sentence_type == 'GGA' and getattr(msg, 'gps_qual', 0) > 0:
@@ -176,8 +172,6 @@ class GnssHardware(Gnss):
                         record.gps_qual = msg.gps_qual
                         record.altitude = msg.altitude
                         record.separation = msg.geo_sep
-                    if getattr(msg, 'spd_over_grnd_kmph', None) is not None:
-                        record.speed_kmh = msg.spd_over_grnd_kmph
                     if msg.sentence_type == 'GNS' and getattr(msg, 'mode_indicator', None):
                         # self.log.info(f'GNS: mode: {msg.mode_indicator}, lat:{msg.latitude} and long:{msg.longitude}')
                         if isinstance(msg.timestamp, datetime.time):
@@ -191,8 +185,7 @@ class GnssHardware(Gnss):
                         record.mode = msg.mode_indicator
                         # print(f'The GNSS message: {msg.mode_indicator}')
                         has_location = True
-                    if msg.sentence_type == 'HDT' and getattr(msg, 'heading', None) is not None:
-                        # self.log.info(f'HDT: Heading: {msg.heading}')
+                    if msg.sentence_type == 'HDT' and getattr(msg, 'heading', None):
                         record.heading = msg.heading
                         has_heading = True
                 except pynmea2.ParseError as e:
@@ -208,7 +201,7 @@ class GnssHardware(Gnss):
         if self.record.gps_qual == 4 and record.gps_qual != 4:
             self.log.info('GNSS RTK fix lost')
             self.RTK_FIX_LOST.emit()
-        self.record = record
+        self.record = deepcopy(record)
         if has_location:
             if record.gps_qual == 4:  # 4 = RTK fixed, 5 = RTK float
                 if self.reference_lat is None or self.reference_lon is None:
