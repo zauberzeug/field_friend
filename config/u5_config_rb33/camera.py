17bc22a942c2c2ebb93b29b45dbaea0760c0ebec
Miguel <angelom93@web.de>
2024-04-18 14:13:45 +0200

    Add configuration files for u5

---
+++ config/u5_config_rb33/camera.py
@@ -0,0 +1,13 @@
+configuration = {'parameters': {
+    'width': 1920,
+    'height': 1080,
+    'auto_exposure': True,
+    'fps': 10,
+},
+    'crop': {
+    'left': 80,
+    'right': 30,
+    'up': 180,
+    'down': 0,
+}
+}
