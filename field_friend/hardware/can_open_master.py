6ea3ab7135c1a3095f3ea9e729da598acb819799
Miguel <angelom93@web.de>
2024-06-03 15:36:39 +0200

    chore: Update restart core interval to 6 seconds in CanOpenMasterHardware

--- field_friend/hardware/can_open_master.py
+++ field_friend/hardware/can_open_master.py
@@ -17,7 +17,7 @@ class CanOpenMasterHardware(rosys.hardware.ModuleHardware):
         ''')
         super().__init__(robot_brain=robot_brain, lizard_code=lizard_code)
         self.message_hooks['error'] = self._on_error
-        rosys.on_repeat(self.restart_core, 10)
+        rosys.on_repeat(self.restart_core, 6)
 
     def _on_error(self, line: str) -> None:
         if 'twai' in line and not self.error:
@@ -26,6 +26,7 @@ class CanOpenMasterHardware(rosys.hardware.ModuleHardware):
 
     async def restart_core(self) -> None:
         if self.error:
-            await self.robot_brain.send('core.restart();')
-            await rosys.sleep(5)
+            self.log.info('Restarting core...')
+            await self.robot_brain.send('core.restart()')
+            await rosys.sleep(3)
             self.error = False
