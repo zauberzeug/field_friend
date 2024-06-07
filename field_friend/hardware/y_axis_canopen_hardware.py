074780c622aae81f18bf47f2aebdf482c14e68a6
Pascal Schade <pascal@zauberzeug.com>
2024-06-07 15:30:59 +0200

    enter velocity mode before referencing

--- field_friend/hardware/y_axis_canopen_hardware.py
+++ field_friend/hardware/y_axis_canopen_hardware.py
@@ -112,6 +112,11 @@ class YAxisCanOpenHardware(YAxis, rosys.hardware.ModuleHardware):
                 f'{self.name}_motor.position_offset = 0;'
             )
             await rosys.sleep(1)
+            self.log.info('activating velocity mode')
+            await self.robot_brain.send(
+                f'{self.name}.speed(0, 0);'
+            )
+            await rosys.sleep(1)
 
             # if in end l stop, move out
             if self.end_l:
