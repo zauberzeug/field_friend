a5eb3030f8c76960348b235a3fafbe3ba98840fd
Rodja Trappe <rodja@zauberzeug.com>
2024-06-20 17:42:38 +0200

    fix e-stop info in header

--- field_friend/interface/components/header_bar.py
+++ field_friend/interface/components/header_bar.py
@@ -29,8 +29,8 @@ class header_bar:
             with ui.row().bind_visibility_from(system.field_friend.estop, 'active').classes('mr-auto bg-red-500 text-white p-2 rounded-md'):
                 ui.icon('report').props('size=md').classes('text-white').props('elevated')
                 ui.label().bind_text_from(system.field_friend.estop, 'pressed_estops',
-                                          lambda e: f'Emergency stop {"/".join(e)} is pressed!') \
-                    .classes('text-white text-3xl').props('elevated')
+                                          lambda e: f'Emergency stop {e} is pressed!') \
+                    .classes('text-white text-xl').props('elevated')
 
             with ui.row().bind_visibility_from(system.field_friend.estop, 'is_soft_estop_active').classes('mr-auto bg-red-500 rounded-md p-1'):
                 ui.icon('report').props('size=md').classes('text-white').props('elevated')
