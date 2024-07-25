bf9e0e603bbf3abe53c98bc6b839496949347538
Pascal Schade <pascal@zauberzeug.com>
2024-07-25 18:14:21 +0200

    fix shadow

--- field_friend/interface/components/status_bulb.py
+++ field_friend/interface/components/status_bulb.py
@@ -8,4 +8,5 @@ class StatusBulb(ValueElement):
         self.on_change()
 
     def on_change(self) -> None:
-        self.style(add='background: radial-gradient(circle at 5px 5px, #5FE5E0, #5898D4); box-shadow: #5FE5E066 0px 0px 10px 5px;' if self.value else 'background: radial-gradient(circle at 5px 5px, #2E5396, #294790);')
+        self.style(add='background: radial-gradient(circle at 5px 5px, #5FE5E0, #5898D4); box-shadow: #5FE5E066 0px 0px 10px 5px;' if self.value
+                   else 'background: radial-gradient(circle at 5px 5px, #2E5396, #294790);', remove='box-shadow:;')
