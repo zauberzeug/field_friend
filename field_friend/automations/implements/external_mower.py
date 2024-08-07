ada269ae15ae0975d53b3e7ad9a3d831210b18f2
Pascal Schade <pascal@zauberzeug.com>
2024-08-07 02:11:49 +0200

    handle stuck motors

--- field_friend/automations/implements/external_mower.py
+++ field_friend/automations/implements/external_mower.py
@@ -1,8 +1,12 @@
 
 
+import logging
 from typing import TYPE_CHECKING
 
+import rosys
+
 from ...hardware import MowerHardware
+from ..navigation import WorkflowException
 from .weeding_implement import Implement
 
 if TYPE_CHECKING:
@@ -12,9 +16,11 @@ if TYPE_CHECKING:
 class ExternalMower(Implement):
     def __init__(self, system: 'System') -> None:
         super().__init__('Mower')
-        self.system = system
+        self.log = logging.getLogger('field_friend.mower')
         self.mower_hardware: MowerHardware = system.field_friend.mower
+        self.driver = system.driver
         assert self.mower_hardware is not None
+        assert self.driver is not None
 
     async def activate(self):
         await self.mower_hardware.turn_on()
@@ -23,3 +29,23 @@ class ExternalMower(Implement):
     async def deactivate(self):
         await self.mower_hardware.turn_off()
         await super().deactivate()
+
+    async def get_stretch(self, max_distance: float) -> float:
+        if not any([self.mower_hardware.m0_error, self.mower_hardware.m1_error, self.mower_hardware.m2_error]):
+            return 0.02
+        if all([self.mower_hardware.m0_error, self.mower_hardware.m1_error, self.mower_hardware.m2_error]):
+            rosys.notify('All motors are stuck', 'negative')
+            raise WorkflowException('All motors are stuck')
+        # TODO: implement a better error handling
+        await rosys.sleep(0.1)
+        self.log.error('Stuck motor detected')
+        await self.mower_hardware.turn_off()
+        await rosys.sleep(2)
+        await self.driver.wheels.drive(*self.driver._throttle(-0.1, 0.0))
+        await rosys.sleep(2)
+        await self.driver.wheels.stop()
+        await rosys.sleep(1)
+        await self.mower_hardware.reset_motors()
+        await rosys.sleep(5)
+        await self.mower_hardware.turn_on()
+        return 0.0
--- field_friend/automations/navigation/__init__.py
+++ field_friend/automations/navigation/__init__.py
@@ -1,12 +1,13 @@
 
 from .coverage_navigation import CoverageNavigation
 from .follow_crops_navigation import FollowCropsNavigation
-from .navigation import Navigation
+from .navigation import Navigation, WorkflowException
 from .row_on_field_navigation import RowsOnFieldNavigation
 from .straight_line_navigation import StraightLineNavigation
 
 __all__ = [
     'Navigation',
+    'WorkflowException',
     'RowsOnFieldNavigation',
     'StraightLineNavigation',
     'FollowCropsNavigation',
