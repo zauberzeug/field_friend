34d97afbd19250c0e7cd3f61bbd7cd00ddaa5ba2
Pascal Schade <pascal@zauberzeug.com>
2024-09-01 02:13:44 +0200

    add zedxmini to sync.py

--- sync.py
+++ sync.py
@@ -7,14 +7,20 @@ from livesync.run_subprocess import run_subprocess
 parser = argparse.ArgumentParser(description='Sync local code with robot.')
 parser.add_argument('robot', help='Robot hostname')
 parser.add_argument('--rosys', action='store_true', default=False, help='Sync rosys')
+parser.add_argument('--zedxmini', action='store_true', default=False, help='Sync zedxmini')
 
 args = parser.parse_args()
 touch = 'touch ~/field_friend/main.py'
-folders = [Folder('.', f'{args.robot}:~/field_friend', on_change=touch),
-           Folder('../zedxmini', f'{args.robot}:~/zedxmini', on_change=touch)]
+folders = [Folder('.', f'{args.robot}:~/field_friend', on_change=touch)]
 if args.rosys:
     folders.append(Folder('../rosys/rosys', f'{args.robot}:~/field_friend/rosys', on_change=touch))
 else:
     print('Ensuring we have no local rosys on the robot')
     run_subprocess(f'ssh {args.robot} "rm -rf ~/field_friend/rosys"')
+
+if args.zedxmini:
+    folders.append(Folder('../zedxmini/zedxmini', f'{args.robot}:~/field_friend/zedxmini', on_change=touch))
+else:
+    print('Ensuring we have no local zedxmini on the robot')
+    run_subprocess(f'ssh {args.robot} "rm -rf ~/field_friend/zedxmini"')
 sync(*folders)
