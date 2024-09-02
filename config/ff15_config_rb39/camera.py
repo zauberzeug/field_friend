0bd1a9e4ec65b8adfc794ccf61b27fb66c45dbd7
Johannes-Thiel <jojo.thadfun@gmail.com>
2024-09-02 14:26:08 +0200

    update to newest config

--- config/ff15_config_rb39/camera.py
+++ config/ff15_config_rb39/camera.py
@@ -1,2 +1,13 @@
-configuration = {
+configuration = {'parameters': {
+    'width': 1280,
+    'height': 720,
+    'auto_exposure': True,
+    'fps': 10,
+},
+    'crop': {
+    'left': 60,
+    'right': 200,
+    'up': 20,
+    'down': 0,
+}
 }
--- config/ff15_config_rb39/pararms.py
+++ config/ff15_config_rb39/pararms.py
@@ -4,5 +4,7 @@ configuration = {
     'pitch': 0.033,
     'wheel_distance': 0.47,
     'antenna_offset': 0.205,
+    'work_x': 0.0125,
+    'drill_radius': 0.025,
     'tool': 'weed_screw',
 }
