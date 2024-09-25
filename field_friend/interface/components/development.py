427c78679c7f6335ae89055fc2e82429b4e6384f
Johannes-Thiel <jojo.thadfun@gmail.com>
2024-09-25 08:32:36 +0200

    test for gpio UI

--- field_friend/interface/components/development.py
+++ field_friend/interface/components/development.py
@@ -8,6 +8,12 @@ from .io_overview import io_overview
 from .settings import settings
 from .status_dev import status_dev_page
 
+# from rosys.hardware import ESPPins
+
+
+# esp_pins: ESPPins
+
+
 if TYPE_CHECKING:
     from field_friend.system import System
 
@@ -26,9 +32,19 @@ def development(system: 'System') -> None:
                                     system.field_friend.robot_brain.developer_ui()
                                 with ui.column():
                                     system.field_friend.robot_brain.communication.debug_ui()
+                                # with ui.row():
+                                #     with ui.card().style('min-width: 200px; background-color: #3E63A6; color: white;'):
+                                #         esp_pins = ESPPins(name='core', robot_brain=system.field_friend.robot_brain)
+                                #         esp_pins.developer_ui()
+
+                                #     # with ui.card().style('min-width: 200px; background-color: #3E63A6; color: white;'):
+                                #     esp_pins = ESPPins(name='p0', robot_brain=system.field_friend.robot_brain)
+                                #     esp_pins.developer_ui()
+
                         else:
                             rosys.simulation_ui()
                     hardware_control(system.field_friend, system.automator, system.puncher)
                     status_dev_page(system.field_friend, system)
     with ui.row().style('width: calc(100vw - 2rem); flex-wrap: nowrap;'):
         io_overview(system)
+        # io_overview(system)
