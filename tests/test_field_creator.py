3a084a97f512a88f9a523e0dc40d3286fad1dfc2
Lukas Baecker <baecker.lukas@outlook.de>
2024-10-09 12:01:43 +0200

    updated field_creator tests

--- tests/test_field_creator.py
+++ tests/test_field_creator.py
@@ -1,41 +1,23 @@
-from rosys.geometry import Point
+from conftest import FIELD_FIRST_ROW_END, FIELD_FIRST_ROW_START
 
-from field_friend import System, localization
+from field_friend import System
 from field_friend.interface.components.field_creator import FieldCreator
-from field_friend.localization import GeoPoint
 
 
-def test_geometry_computation(system: System):
-    field_creator = FieldCreator(system)
-    localization.reference = GeoPoint(lat=51.98317071260942, long=7.43411239981148)
-    first_row_start = GeoPoint(lat=51.98317071260942, long=7.43411239981148)
-    first_row_end = first_row_start.shifted(Point(x=0, y=10))
-    field_creator.first_row_start = first_row_start
-    field_creator.row_spacing = 0.5
-    field_creator.row_number = 10
-    field_creator.confirm_geometry()
-    field_creator.first_row_end = first_row_end
+def test_geometry_computation(system: System, field_creator: FieldCreator):
     assert len(system.field_provider.fields) == 0
     field_creator.next()
     assert len(system.field_provider.fields) == 1
     assert system.field_provider.fields[0].name == 'Field 1'
     assert system.field_provider.fields[0].row_spacing == 0.5
     assert system.field_provider.fields[0].row_number == 10
-    assert system.field_provider.fields[0].first_row_start == first_row_start
-    assert system.field_provider.fields[0].first_row_end == first_row_end
-    assert len(system.field_provider.fields[0].rows) == 11
+    assert system.field_provider.fields[0].first_row_start == FIELD_FIRST_ROW_START
+    assert system.field_provider.fields[0].first_row_end == FIELD_FIRST_ROW_END
+    assert len(system.field_provider.fields[0].rows) == 10
 
 
-def test_wrong_row_spacing(system: System):
-    field_creator = FieldCreator(system)
-    localization.reference = GeoPoint(lat=51.98317071260942, long=7.43411239981148)
-    first_row_start = GeoPoint(lat=51.98317071260942, long=7.43411239981148)
-    first_row_end = first_row_start.shifted(Point(x=0, y=10))
-    field_creator.first_row_start = first_row_start
-    field_creator.row_spacing = 0.5
-    field_creator.row_number = 10
-    field_creator.confirm_geometry()
-    field_creator.first_row_end = first_row_end
+def test_wrong_row_spacing(system: System, field_creator: FieldCreator):
+    field_creator.row_spacing = 0
     assert len(system.field_provider.fields) == 0
     field_creator.next()
     assert len(system.field_provider.fields) == 1
