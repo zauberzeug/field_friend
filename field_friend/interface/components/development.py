f65a27e678460c64bd28b39cd5b2dbd5f81415db
Pascal Schade <pascal@zauberzeug.com>
2024-10-16 18:07:41 +0200

    add esppins

--- field_friend/interface/components/development.py
+++ field_friend/interface/components/development.py
@@ -2,6 +2,7 @@ from typing import TYPE_CHECKING
 
 import rosys
 from nicegui import ui
+from rosys.hardware import EspPins
 
 from .hardware_control import hardware_control
 from .io_overview import io_overview
@@ -30,5 +31,13 @@ def development(system: 'System') -> None:
                             rosys.simulation_ui()
                     hardware_control(system.field_friend, system.automator, system.puncher)
                     status_dev_page(system.field_friend, system)
+    if isinstance(system.field_friend, rosys.hardware.RobotHardware):
+        with ui.row():
+            with ui.card().style('min-width: 200px;'):
+                esp_pins_core = EspPins(name='core', robot_brain=system.field_friend.robot_brain)
+                esp_pins_core.developer_ui()
+            with ui.card().style('min-width: 200px;'):
+                esp_pins_p0 = EspPins(name='p0', robot_brain=system.field_friend.robot_brain)
+                esp_pins_p0.developer_ui()
     with ui.row().style('width: calc(100vw - 2rem); flex-wrap: nowrap;'):
         io_overview(system)
