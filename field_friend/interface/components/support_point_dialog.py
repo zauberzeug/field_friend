da1cd5c2889ff8d7947d0790b1d522bde45578fb
LukasBaecker <65940705+LukasBaecker@users.noreply.github.com>
2024-10-21 16:09:13 +0200

    Update field_friend/interface/components/support_point_dialog.py
    Co-authored-by: Pascal Schade <165774906+pascalzauberzeug@users.noreply.github.com>

--- field_friend/interface/components/support_point_dialog.py
+++ field_friend/interface/components/support_point_dialog.py
@@ -48,7 +48,7 @@ class SupportPointDialog:
                 'text-lg')
             ui.label('2. Enter the row number for the support point:').classes('text-lg')
             ui.number(
-                label='Row Number', min=1, max=self.field_provider.fields[0].row_number - 1 if self.field_provider.fields else 1, step=1, value=1) \
+                label='Row Number', min=1, max=self.field_provider.fields[0].row_number if self.field_provider.fields else 1, step=1, value=1) \
                 .props('dense outlined').classes('w-40') \
                 .tooltip('Choose the row number you would like to give a fixed support point to.') \
                 .bind_value(self, 'row_name')
