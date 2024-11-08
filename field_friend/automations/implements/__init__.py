ab3a80016132d88f3a6922638f7e52eb2496ab2b
Paula Kammler <paula@zauberzeug.com>
2024-11-08 17:05:48 +0100

    refactor(chop_and_screw): delete it, since it's not used

--- field_friend/automations/implements/__init__.py
+++ field_friend/automations/implements/__init__.py
@@ -1,17 +1,13 @@
-# isort: off
-from .weeding_implement import WeedingImplement, ImplementException
-# isort: on
-from .chop_and_screw import ChopAndScrew
 from .external_mower import ExternalMower
 from .implement import Implement
 from .recorder import Recorder
 from .tornado import Tornado
+from .weeding_implement import ImplementException, WeedingImplement
 from .weeding_screw import WeedingScrew
 
 __all__ = [
     'Implement',
     'WeedingImplement',
-    'ChopAndScrew',
     'Recorder',
     'WeedingScrew',
     'Tornado',
