457dea3b98d936b3cf7309565826ef972df5b778
SeaTechRC <153106523+SeaTechRC@users.noreply.github.com>
2024-11-08 15:26:09 +0100

    Fix edge-case for _generate_outline for field (#224)
    Hello,
    
    Small fix for the 1 row edge-case for field 1-row _generate_outline.
    
    ---------
    
    Co-authored-by: Pascal Schade <165774906+pascalzauberzeug@users.noreply.github.com>

--- field_friend/automations/field.py
+++ field_friend/automations/field.py
@@ -144,14 +144,18 @@ class Field:
 
     def _generate_outline(self) -> list[GeoPoint]:
         assert len(self.rows) > 0
-        outline_unbuffered: list[Point] = []
-        for p in self.rows[-1].points:
-            outline_unbuffered.append(p.cartesian())
-        outline_unbuffered.append(self.first_row_end.cartesian())
-        outline_unbuffered.append(self.first_row_start.cartesian())
-        outline_polygon = Polygon([p.tuple for p in outline_unbuffered])
-        bufferd_polygon = outline_polygon.buffer(
-            self.outline_buffer_width, join_style='mitre', mitre_limit=math.inf)
+        outline_unbuffered: list[Point] = [
+            self.first_row_end.cartesian(),
+            self.first_row_start.cartesian()
+        ]
+        if len(self.rows) > 1:
+            for p in self.rows[-1].points:
+                outline_unbuffered.append(p.cartesian())
+            outline_shape = Polygon([p.tuple for p in outline_unbuffered])
+        else:
+            outline_shape = LineString([p.tuple for p in outline_unbuffered])
+        bufferd_polygon = outline_shape.buffer(
+            self.outline_buffer_width, cap_style='square', join_style='mitre', mitre_limit=math.inf)
         bufferd_polygon_coords = bufferd_polygon.exterior.coords
         outline: list[GeoPoint] = [localization.reference.shifted(
             Point(x=p[0], y=p[1])) for p in bufferd_polygon_coords]
