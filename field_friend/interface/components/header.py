93db7845bccb4d88aa9537762adf92752a763f8c
Paula Kammler <paula@zauberzeug.com>
2024-11-13 11:09:44 +0100

    move content of system_bar to dev page

--- field_friend/interface/components/header.py
+++ field_friend/interface/components/header.py
@@ -4,7 +4,6 @@ from typing import TYPE_CHECKING
 
 from .header_bar import HeaderBar as header_bar
 from .status_drawer import create_status_drawer
-from .system_bar import SystemBar as system_bar
 
 if TYPE_CHECKING:
     from ...system import System
@@ -13,4 +12,3 @@ if TYPE_CHECKING:
 def create_header(system: System) -> None:
     drawer = create_status_drawer(system)
     header_bar(system, drawer)
-    system_bar()
--- field_friend/interface/dev_page.py
+++ field_friend/interface/dev_page.py
@@ -1,3 +1,7 @@
+from datetime import timedelta
+
+import psutil
+import rosys
 from nicegui import ui
 
 from ..system import System
@@ -13,3 +17,12 @@ class DevPage:
         def page() -> None:
             create_header(system)
             create_development_ui(system)
+
+            with ui.footer():
+                timer = ui.label().classes('flex-grow')
+                cpu_label = ui.label().classes('flex-grow')
+
+                async def update_status() -> None:
+                    timer.set_text(f'{timedelta(seconds=rosys.uptime())}')
+                    cpu_label.set_text(f'CPU: {psutil.cpu_percent():.0f}%')
+                ui.timer(rosys.config.ui_update_interval, update_status)
