ea858d8235c3dac58d5024ba94a9f68ecc407418
Johannes-Thiel <jojo.thadfun@gmail.com>
2024-11-29 14:26:48 +0100

    add config for f16

---
+++ config/f16_config_rb43/hardware.py
@@ -0,0 +1,66 @@
+configuration = {
+    'wheels': {
+        'version': 'double_wheels',
+        'name': 'wheels',
+        'left_back_can_address': 0x100,
+        'left_front_can_address': 0x000,
+        'right_back_can_address': 0x300,
+        'right_front_can_address': 0x200,
+        'is_left_reversed': True,
+        'is_right_reversed': False,
+        'odrive_version': 6,
+    },
+    'y_axis': {
+        'version': 'none',
+    },
+    'flashlight': {
+        'version': 'none',
+    },
+
+    'z_axis': {
+        'version': 'none',
+    },
+    'estop': {
+        'name': 'estop',
+        'pins': {'1': 34, '2': 35},
+    },
+    'bms': {
+        'name': 'bms',
+        'on_expander': True,
+        'rx_pin': 26,
+        'tx_pin': 27,
+        'baud': 9600,
+        'num': 2,
+    },
+    'battery_control': {
+        'name': 'battery_control',
+        'on_expander': True,
+        'reset_pin': 15,
+        'status_pin': 13,
+    },
+    'status_control': {
+        'name': 'status_control',
+    },
+    'bluetooth': {
+        'name': 'fieldfriend-f16',
+    },
+    'serial': {
+        'name': 'serial',
+        'rx_pin': 26,
+        'tx_pin': 27,
+        'baud': 115200,
+        'num': 1,
+    },
+    'expander': {
+        'name': 'p0',
+        'boot': 25,
+        'enable': 14,
+    },
+    'can': {
+        'name': 'can',
+        'on_expander': False,
+        'rx_pin': 32,
+        'tx_pin': 33,
+        'baud': 1_000_000,
+    },
+}
