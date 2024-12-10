9275296e4b6a14663e9c5d7b82fd56f761dc8341
Lukas Baecker <baecker.lukas@outlook.de>
2024-12-10 11:15:34 +0100

    soft e-stop message added

--- field_friend/app_controls.py
+++ field_friend/app_controls.py
@@ -17,31 +17,45 @@ class AppControls(RosysAppControls):
         self.field_friend = field_friend
         self.last_battery_percentage: float | None = self.field_friend.bms.state.percentage
         self.last_charging: bool | None = self.field_friend.bms.state.is_charging
+        self.last_estop_active: bool = self.field_friend.estop.active
+        self.last_estop_soft_active: bool = self.field_friend.estop.is_soft_estop_active
+        self.last_bumpers_active: list[str] = []
         self.last_info: str = ''
         self.APP_CONNECTED.register(self.reset)
-        if self.field_friend.bumper:
-            self.field_friend.bumper.BUMPER_TRIGGERED.register(self.sync)
-        self.field_friend.estop.ESTOP_TRIGGERED.register(self.sync)
-        rosys.on_repeat(self.check_battery, 5.0)
+        rosys.on_repeat(self.check_status, 2.0)
 
-    async def check_battery(self) -> None:
-        if self.field_friend.bms.state.percentage != self.last_battery_percentage or self.field_friend.bms.state.is_charging != self.last_charging:
+    async def check_status(self) -> None:
+        estop_changed = self.field_friend.estop.active != self.last_estop_active or self.field_friend.estop.is_soft_estop_active != self.last_estop_soft_active
+        if estop_changed:
+            self.last_estop_active = self.field_friend.estop.active
+            self.last_estop_pressed = self.field_friend.estop.pressed_estops
+            self.last_estop_soft_active = self.field_friend.estop.is_soft_estop_active
+        battery_changed = self.field_friend.bms.state.percentage != self.last_battery_percentage or self.field_friend.bms.state.is_charging != self.last_charging
+        if battery_changed:
             self.last_battery_percentage = self.field_friend.bms.state.percentage
             self.last_charging = self.field_friend.bms.state.is_charging
+        bumpers_changed = self.last_bumpers_active != self.field_friend.bumper.active_bumpers if self.field_friend.bumper else False
+        if bumpers_changed:
+            assert self.field_friend.bumper is not None
+            self.last_bumpers_active = self.field_friend.bumper.active_bumpers
+        if estop_changed or battery_changed or bumpers_changed:
+            print(self.field_friend.estop.pressed_estops)
             await self.sync()
 
     async def sync(self) -> None:
         await super().sync()
         infos = []
         if self.field_friend.estop.active:
-            infos.append('E Stop active')
-        if self.field_friend.bumper and self.field_friend.bumper.active_bumpers:
-            infos.append(f'Bumper ({" ".join(self.field_friend.bumper.active_bumpers)}) active')
-        if self.field_friend.bms.state.is_charging:
-            infos.append('charging battery')
-        battery = f'{self.field_friend.bms.state.percentage:.0f}%' if self.field_friend.bms.state.percentage is not None else '?'
-        infos.append(f'Battery: {battery}')
-
+            infos.append('One or multiple E-Stops active. Please check the robot.')
+        elif self.field_friend.estop.is_soft_estop_active:
+            infos.append('Soft E-Stop active. Please check the robot.')
+        elif self.field_friend.bumper and self.field_friend.bumper.active_bumpers:
+            infos.append('A bumper is active. Please check the robot.')
+        else:
+            if self.field_friend.bms.state.is_charging:
+                infos.append('charging battery')
+            battery = f'{self.field_friend.bms.state.percentage:.0f}%' if self.field_friend.bms.state.percentage is not None else '?'
+            infos.append(f'Battery: {battery}')
         info = ' | '.join(infos)
         if info != self.last_info:
             await self.set_info(info)
