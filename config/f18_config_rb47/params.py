b268630e22a5d53bb20cc3617ba66be301ba4007
Pascal Schade <pascal@zauberzeug.com>
2024-12-11 22:06:34 +0100

    set work_x

--- config/f18_config_rb47/params.py
+++ config/f18_config_rb47/params.py
@@ -3,7 +3,7 @@ configuration = {
     'thooth_count': 15,
     'pitch': 0.033,
     'wheel_distance': 0.47,
-    'work_x': 0.093,
+    'work_x': 0.12,
     'drill_radius': 0.025,
     'antenna_offset': 0.205,
     'tool': 'weed_screw',
