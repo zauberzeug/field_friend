6f259341155b44b7462f6cd798a752879d28a537
Johannes T. <119115663+Johannes-Thiel@users.noreply.github.com>
2025-01-09 16:13:03 +0100

    Add F10 Config (#252)
    while migrating the configs the config for f10 was lost. This is a
    restoration of the config

---
+++ config/ff10XL_config_rb30/params.py
@@ -0,0 +1,10 @@
+configuration = {
+    'motor_gear_ratio': 12.52,
+    'thooth_count': 17,
+    'pitch': 0.041,
+    'wheel_distance': 0.47,
+    'work_x': 0.07,
+    'drill_radius': 0.025,
+    'tool': 'none',
+    'antenna_offset': 0.205,
+}
