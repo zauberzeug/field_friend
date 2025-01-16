4c55a537fcaadafc3a483a63be55aea3e990deeb
Paula Kammler <paula@zauberzeug.com>
2025-01-16 15:29:01 +0100

    fix typo

--- config/__init__.py
+++ config/__init__.py
@@ -28,7 +28,7 @@ def get_config(robot_name: str) -> FieldFriendConfiguration:
     try:
         module_name = f'config.{robot_name.lower()}'
         config_module = importlib.import_module(module_name)
-        return config_module.conf
+        return config_module.config
     except ImportError as e:
         raise RuntimeError(f'No configuration found for robot: {robot_name}') from e
 
