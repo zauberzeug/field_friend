6419e381c6724b178257f224e4c5a901494c0002
Pascal Schade <165774906+pascalzauberzeug@users.noreply.github.com>
2025-01-23 18:32:09 +0100

    Add IMU to Kalman Filter (#259)
    - [x] Real life tests
    - [x] Implement IMU changes in
    https://github.com/zauberzeug/rosys/tree/rework_imu
    - [x] Revert U4 config changes when Lizard is fixed (Tests fail because
    tornado is deactivated)
    - [x] Wait for https://github.com/zauberzeug/rosys/pull/250 and [RoSys
    0.22.0](https://github.com/zauberzeug/rosys/milestone/38)

--- field_friend/hardware/imu.py
+++
@@ -1,9 +0,0 @@
-import rosys
-
-
-class IMUHardware(rosys.hardware.ModuleHardware):
-
-    def __init__(self, robot_brain: rosys.hardware.RobotBrain, *,
-                 name: str = 'imu') -> None:
-        lizard_code = f'{name} = Imu()'
-        super().__init__(robot_brain, lizard_code=lizard_code)
