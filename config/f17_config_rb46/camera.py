1ae8677780dd516f07ef339120f58eabc698d127
Johannes-Thiel <jojo.thadfun@gmail.com>
2025-01-30 13:54:51 +0100

    add camera setup

--- config/f17_config_rb46/camera.py
+++ config/f17_config_rb46/camera.py
@@ -4,4 +4,10 @@ configuration = {
         'height': 1080,
         'auto_exposure': True,
     },
+    'circle_sight_positions': {
+        'front': '-1',
+        'back': '-2',
+        'left': '-3',
+        'right': '-4',
+    }
 }
