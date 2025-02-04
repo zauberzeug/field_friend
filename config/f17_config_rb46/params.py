52fc32e18820fc99d5be6ed30b6160c9d103b858
Pascal Schade <165774906+pascalzauberzeug@users.noreply.github.com>
2025-02-04 14:42:52 +0100

    Update config/f17_config_rb46/params.py

--- config/f17_config_rb46/params.py
+++ config/f17_config_rb46/params.py
@@ -4,8 +4,8 @@ configuration = {
     'pitch': 0.033,
     'wheel_distance': 1.5,
     'antenna_offset': 0,
-    'work_x': -0.06933333,
-    'work_y': 0.0094166667,
+    'work_x': 0.0,
+    'work_y': 0.0,
     'drill_radius': 0.025,
     'tool': 'none',
 }
