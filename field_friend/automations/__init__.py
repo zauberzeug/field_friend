26031a46eda2065b964b3f243392dc97b9b31f7d
Pascal Schade <pascal@zauberzeug.com>
2025-03-13 08:13:54 +0100

    Add EntityLocator base class

--- field_friend/automations/__init__.py
+++ field_friend/automations/__init__.py
@@ -7,13 +7,14 @@ from .implements.implement import Implement
 from .kpi_provider import KpiProvider
 from .path_provider import Path, PathProvider
 from .plant import Plant
-from .plant_locator import PlantLocator
+from .plant_locator import EntityLocator, PlantLocator
 from .plant_provider import PlantProvider
 from .puncher import Puncher
 
 __all__ = [
     'AutomationWatcher',
     'BatteryWatcher',
+    'EntityLocator',
     'Field',
     'FieldProvider',
     'Implement',
