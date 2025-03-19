c2f87f4a22e4fb9d0faf9853fa170823832a231c
Pascal Schade <165774906+pascalzauberzeug@users.noreply.github.com>
2025-03-19 13:42:49 +0100

    Add demo mode for weeding screw (#295)
    Previously the demo mode was only available for the tornado implement. I
    also adjusted the `work_x` offset for U6.

--- config/u6_config_rb34/params.py
+++ config/u6_config_rb34/params.py
@@ -3,7 +3,7 @@ configuration = {
     'thooth_count': 17,
     'pitch': 0.041,
     'wheel_distance': 0.47,
-    'work_x': 0.085,
+    'work_x': 0.11,
     'drill_radius': 0.025,
     'antenna_offset': 0.205,
     'tool': 'weed_screw',
