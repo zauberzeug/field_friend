eeaa93664b658eea28b1505078c3d73bfb5ff6ab
Pascal Schade <pascal@zauberzeug.com>
2025-04-01 13:31:53 +0200

    add brain id to status

--- field_friend/api/status.py
+++ field_friend/api/status.py
@@ -37,6 +37,7 @@ class Status:
             position = self.system.gnss.last_measurement.point.degree_tuple if self.system.gnss.last_measurement is not None else None
             data = {
                 'robot_id': self.system.robot_id,
+                'brain_id': self.system.config.robot_brain.name,
                 'battery': self.system.field_friend.bms.state.percentage,
                 'battery_charging': self.system.field_friend.bms.state.is_charging,
                 'status': work_status,
