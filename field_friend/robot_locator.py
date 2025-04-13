8ffa7f81fc56cccb2bd637baa70df3d0a42c65db
Pascal Schade <pascal@zauberzeug.com>
2025-04-14 00:49:27 +0200

    correctly set ignore flags to True if sensor is none

--- field_friend/robot_locator.py
+++ field_friend/robot_locator.py
@@ -32,8 +32,8 @@ class RobotLocator(rosys.persistence.PersistentModule):
         self._pose_timestamp = rosys.time()
 
         # bound attributes
-        self._ignore_gnss = False
-        self._ignore_imu = False
+        self._ignore_gnss = True if gnss is None else False
+        self._ignore_imu = True if imu is None else False
         self._r_odom_linear = self.R_ODOM_LINEAR
         self._r_odom_angular = self.R_ODOM_ANGULAR
         self._r_imu_angular = self.R_IMU_ANGULAR
