28a4b473faf72e415ecc518296623551925569a9
Johannes-Thiel <jojo.thadfun@gmail.com>
2025-04-16 08:50:17 +0200

    add low bandwidth

---
+++ field_friend/interface/low_bandwidth_page.py
@@ -0,0 +1,44 @@
+import rosys
+from nicegui import binding, ui
+from rosys.analysis import track
+
+from ..system import System
+from .components import CameraCard as camera_card
+from .components import LeafletMap as leaflet_map
+from .components import Operation as operation
+from .components import RobotScene as robot_scene
+from .components import create_header
+from .components.log_monitor import LogMonitor
+
+
+class LowBandwidthPage:
+
+    def __init__(self, system: System) -> None:
+        self.system = system
+        self.log_monitor = LogMonitor()
+
+        @ui.page('/lb')
+        def page() -> None:
+            create_header(system)
+            self.content(devmode=False)
+
+    def content(self, devmode: bool) -> None:
+        page_height = '50vh' if devmode else 'calc(100vh - 170px)'
+        ui.colors(primary='#6E93D6', secondary='#53B689', accent='#111B1E', positive='#53B689')
+        with ui.row().style(f'height:{page_height}; width: calc(100vw - 2rem); flex-wrap: nowrap;'):
+            with ui.row().classes('h-full ml-2 m-2').style('width: calc(100% - 1rem)'):
+                operation(self.system)
+                with ui.column().classes('h-full').style('width: calc(45% - 2rem); flex-wrap: nowrap;'):
+                    with ui.row().style('margin: 1rem; width: calc(100% - 2rem);'):
+                        with ui.column():
+                            ui.button('emergency stop', on_click=lambda: self.system.field_friend.estop.set_soft_estop(True)).props('color=red') \
+                                .classes('py-3 px-6 text-lg').bind_visibility_from(self.system.field_friend.estop, 'is_soft_estop_active', value=False)
+                            ui.button('emergency reset', on_click=lambda: self.system.field_friend.estop.set_soft_estop(False)) \
+                                .props('color=red-700 outline').classes('py-3 px-6 text-lg') \
+                                .bind_visibility_from(self.system.field_friend.estop, 'is_soft_estop_active', value=True)
+                        ui.space()
+                        with ui.row():
+                            rosys.automation.automation_controls(self.system.automator)
+            with ui.column():
+                track.ui()
+                self.log_monitor.ui()
