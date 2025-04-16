9365d94b3b520f62adb01f826519548e7fed9081
Johannes-Thiel <jojo.thadfun@gmail.com>
2025-04-16 13:48:09 +0200

    fix page and add info labels for navigation

--- field_friend/interface/low_bandwidth_page.py
+++ field_friend/interface/low_bandwidth_page.py
@@ -1,4 +1,3 @@
-# pylint: disable=duplicate-code
 import rosys
 from nicegui import ui
 from rosys.analysis import track
@@ -24,19 +23,22 @@ class LowBandwidthPage:
         page_height = '50vh' if devmode else 'calc(100vh - 170px)'
         ui.colors(primary='#6E93D6', secondary='#53B689', accent='#111B1E', positive='#53B689')
         with ui.row().style(f'height:{page_height}; width: calc(100vw - 2rem); flex-wrap: nowrap;'):
-            with ui.row().classes('h-full ml-2 m-2').style('width: calc(100% - 1rem)'):
-                operation(self.system)
-                with ui.column().classes('h-full').style('width: calc(45% - 2rem); flex-wrap: nowrap;'):
-                    with ui.row().style('margin: 1rem; width: calc(100% - 2rem);'):
-                        with ui.column():
-                            ui.button('emergency stop', on_click=lambda: self.system.field_friend.estop.set_soft_estop(True)).props('color=red') \
-                                .classes('py-3 px-6 text-lg').bind_visibility_from(self.system.field_friend.estop, 'is_soft_estop_active', value=False)
-                            ui.button('emergency reset', on_click=lambda: self.system.field_friend.estop.set_soft_estop(False)) \
-                                .props('color=red-700 outline').classes('py-3 px-6 text-lg') \
-                                .bind_visibility_from(self.system.field_friend.estop, 'is_soft_estop_active', value=True)
-                        ui.space()
+            operation(self.system)
+            with ui.column().classes('h-full').style('width: calc(45% - 2rem); flex-wrap: nowrap;'):
+                with ui.row().style('margin: 1rem; width: calc(100% - 2rem);'):
+                    with ui.column():
+                        ui.button('emergency stop', on_click=lambda: self.system.field_friend.estop.set_soft_estop(True)).props('color=red') \
+                            .classes('py-3 px-6 text-lg').bind_visibility_from(self.system.field_friend.estop, 'is_soft_estop_active', value=False)
+                        ui.button('emergency reset', on_click=lambda: self.system.field_friend.estop.set_soft_estop(False)) \
+                            .props('color=red-700 outline').classes('py-3 px-6 text-lg') \
+                            .bind_visibility_from(self.system.field_friend.estop, 'is_soft_estop_active', value=True)
                         with ui.row():
                             rosys.automation.automation_controls(self.system.automator)
+                    with ui.column().bind_visibility_from(self.system.automator, 'is_running'):
+                        ui.label('').bind_text_from(self.system.current_navigation,
+                                                    '_state', lambda state: f'State: {state.name}')
+                        ui.label('').bind_text_from(self.system.current_navigation,
+                                                    'row_index', lambda row_index: f'Row Index: {row_index}')
             with ui.column():
                 track.ui()
                 self.log_monitor.ui()
