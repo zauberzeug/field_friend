346f4d35a5eb18aa8830acad0693614769805391
Pascal Schade <pascal@zauberzeug.com>
2025-04-17 11:38:57 +0200

    cleanup logging

--- field_friend/interface/components/camera_card.py
+++ field_friend/interface/components/camera_card.py
@@ -233,7 +233,6 @@ class CameraCard:
         svg = ''
         tool_3d = rosys.geometry.Pose3d(x=self.field_friend.WORK_X, y=self.field_friend.y_axis.position, z=0).in_frame(
             self.robot_locator.pose_frame).resolve().point_3d
-        self.log.warning(f'tool_3d: {tool_3d}')
         tool_2d = self.camera.calibration.project_to_image(tool_3d)
         if tool_2d:
             tool_2d = tool_2d / self.shrink_factor
