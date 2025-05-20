eb62e19c9ef4c1dda316cd184e68663825f31077
Pascal Schade <pascal@zauberzeug.com>
2025-05-20 16:45:14 +0200

    wait for imu measurement in simulation

--- field_friend/robot_locator.py
+++ field_friend/robot_locator.py
@@ -96,6 +96,8 @@ class RobotLocator(rosys.persistence.Persistable):
             dt = velocity.time - self._pose_timestamp
             self._pose_timestamp = velocity.time
             if (not self._first_prediction_done) and (self._imu is not None):
+                if self._imu.last_measurement is None:
+                    return
                 self._previous_imu_measurement = self._imu.last_measurement
 
             if velocity.linear == 0 and velocity.angular == 0 and self._first_prediction_done:
