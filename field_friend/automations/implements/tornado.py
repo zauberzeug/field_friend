49158796ed55e24bb7e5223cebc6d0565bc6408b
Pascal Schade <pascal@zauberzeug.com>
2025-05-21 03:00:03 +0200

    remove tornado look ahead check

--- field_friend/automations/implements/tornado.py
+++ field_friend/automations/implements/tornado.py
@@ -15,8 +15,6 @@ if TYPE_CHECKING:
 
 
 class Tornado(WeedingImplement):
-    LOOK_AHEAD_DISTANCE: float = 0.25
-
     def __init__(self, system: System) -> None:
         super().__init__('Tornado', system)
         self.tornado_angle: float = 30.0
@@ -71,9 +69,6 @@ class Tornado(WeedingImplement):
             return None
         if not self.system.field_friend.can_reach(closest_crop_position.projection()):
             self.log.debug('Target crop is not in the working area')
-            return None
-        if closest_crop_position.x >= self.system.field_friend.WORK_X + self.LOOK_AHEAD_DISTANCE:
-            self.log.debug('Closest crop not yet in range')
             return None
         if self._crops_in_drill_range(closest_crop_id, closest_crop_position.projection(), self.tornado_angle):
             self.log.debug('Crops in drill range')
