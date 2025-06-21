7bbbe80b33d32544c95821f3fbce3f63bdab1533
Rodja Trappe <rodja@zauberzeug.com>
2025-06-21 08:26:48 +0200

    add stricter asserts in test_straight_line_with_high_angles

--- tests/test_navigation.py
+++ tests/test_navigation.py
@@ -51,13 +51,15 @@ async def test_straight_line_with_high_angles(system: System):
     await forward(until=lambda: abs(rosys.helpers.angle(system.robot_locator.pose.yaw, target_yaw)) < np.deg2rad(0.1))
     await system.driver.wheels.stop()
     assert isinstance(system.current_navigation, StraightLineNavigation)
+    assert isinstance(system.current_navigation.implement, Recorder)
     system.current_navigation.length = 1.0
     system.automator.start()
     await forward(until=lambda: system.automator.is_running)
-    await forward(until=lambda: system.automator.is_stopped)
+    await forward(until=lambda: system.automator.is_stopped, timeout=24)
     assert system.robot_locator.pose.point.x == pytest.approx(-0.985, abs=0.1)
     assert system.robot_locator.pose.point.y == pytest.approx(-0.174, abs=0.1)
     assert system.robot_locator.pose.yaw_deg == pytest.approx(predicted_yaw, abs=5)
+
 
 @pytest.mark.parametrize('target, end_pose, max_turn_angle', [
     (rosys.geometry.Point(x=1.0, y=0.0), rosys.geometry.Pose(x=1.0, y=0.0, yaw=0.0), 1.0),
