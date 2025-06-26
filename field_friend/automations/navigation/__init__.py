79842f0eeea5c55db137b01b2faef59ac5de2318
Pascal Schade <pascal@zauberzeug.com>
2025-06-26 19:30:14 +0200

    remove follow crops for now

--- field_friend/automations/navigation/__init__.py
+++ field_friend/automations/navigation/__init__.py
@@ -1,6 +1,5 @@
 from .crossglide_demo_navigation import CrossglideDemoNavigation
 from .field_navigation import FieldNavigation
-from .follow_crops_navigation import FollowCropsNavigation
 from .navigation import Navigation, WorkflowException
 from .straight_line_navigation import StraightLineNavigation
 from .waypoint_navigation import WaypointNavigation, WorkingSegment
@@ -8,7 +7,6 @@ from .waypoint_navigation import WaypointNavigation, WorkingSegment
 __all__ = [
     'CrossglideDemoNavigation',
     'FieldNavigation',
-    'FollowCropsNavigation',
     'Navigation',
     'StraightLineNavigation',
     'WaypointNavigation',
