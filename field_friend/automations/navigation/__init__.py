23e9d3217846cc35f68058cdef709bb2bfe876cc
Pascal Schade <pascal@zauberzeug.com>
2025-08-04 09:18:16 +0200

    make is_reference_valid public

--- field_friend/automations/navigation/__init__.py
+++ field_friend/automations/navigation/__init__.py
@@ -1,7 +1,7 @@
 from .field_navigation import FieldNavigation, RowSegment
 from .implement_demo_navigation import ImplementDemoNavigation
 from .straight_line_navigation import StraightLineNavigation
-from .waypoint_navigation import DriveSegment, WaypointNavigation, WorkflowException
+from .waypoint_navigation import DriveSegment, WaypointNavigation, WorkflowException, is_reference_valid
 
 __all__ = [
     'DriveSegment',
@@ -11,4 +11,5 @@ __all__ = [
     'StraightLineNavigation',
     'WaypointNavigation',
     'WorkflowException',
+    'is_reference_valid',
 ]
