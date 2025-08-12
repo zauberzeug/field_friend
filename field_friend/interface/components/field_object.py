7b75fa1f6c676396c5179c08a91f3a4d91ababd1
Pascal Schade <pascal@zauberzeug.com>
2025-08-13 01:31:57 +0200

    make charging markers smaller

--- field_friend/interface/components/field_object.py
+++ field_friend/interface/components/field_object.py
@@ -94,8 +94,8 @@ class FieldObject(Group):
         if active_field.charge_dock_pose is not None:
             assert active_field.charge_approach_pose is not None
             local_docked_pose = active_field.charge_dock_pose.to_local()
-            Sphere(radius=0.1).move(x=local_docked_pose.x, y=local_docked_pose.y, z=0.1) \
+            Sphere(radius=0.05).move(x=local_docked_pose.x, y=local_docked_pose.y, z=0.1) \
                 .material('#ff0000').with_name('docking_station')
             local_approach_pose = active_field.charge_approach_pose.to_local()
-            Sphere(radius=0.1).move(x=local_approach_pose.x, y=local_approach_pose.y, z=0.1) \
+            Sphere(radius=0.05).move(x=local_approach_pose.x, y=local_approach_pose.y, z=0.1) \
                 .material('#008000').with_name('docking_approach')
